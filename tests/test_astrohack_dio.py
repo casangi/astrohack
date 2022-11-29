15c4b33a3e4093e5023bc0d2e364cf64e0d2291d
Joshua Hoskins <jrh4dy@gmail.com>
2022-11-29 08:43:38 -0500

    Finished tests for dio of pointing dictionary and hackfile loading. Finished setting up automated temp directory creation.

--- tests/test_astrohack_dio.py
+++ tests/test_astrohack_dio.py
@@ -2,6 +2,8 @@ import os
 import gdown
 import tarfile
 import pytest
+import shutil
+import pathlib
 
 import numpy as np
 
@@ -12,31 +14,48 @@ from astrohack._utils._io import _load_pnt_dict
 
 @pytest.fixture(scope='session')
 def make_test_directory(tmp_path_factory):
-    
-    base_path = "/".join((os.getcwd(), "data"))
+    """ Provides temporary data directories valid for the length of the testing session; handles 
+        post-test clean up automatically.
 
+    Args:
+        tmp_path_factory (fixture): session-scoped fixture which can be used to create arbitrary 
+                                    temporary directories from any other fixture or test.
+
+    Returns:
+        pathlib.Path: path object describing the location of the temporary directory.
+    """
+    
+    # Setup an initial path to check against.
+    base_path = pathlib.PosixPath("/".join((os.getcwd(), "data")))
+
+    # If the temporary directory exists, return path.
     if os.path.exists(base_path):
         return base_path
 
+    # Make temporary path object
     base_path = tmp_path_factory.mktemp("data")
 
     url = 'https://drive.google.com/u/0/uc?id=1PqLmoDXzv8vl0rXWTMTyYRyJuj6E1fCD'
     output = 'hackfile.tar'
 
+    # Download data from public gdrive
     gdown.download(url, output)
 
     tar = tarfile.open( "/".join((os.getcwd(), output)) )
     tar.extractall(path=base_path)
     tar.close()
+    shutil.move(output, base_path/output)
 
     url = 'https://drive.google.com/u/0/uc?id=1d7ARSgO2DwmXSUf7T1IbC8t5yYa3dbGr'
     output = 'test.pnt.dict.tar'
 
+    # Download data from public gdrive
     gdown.download(url, output)
  
     tar = tarfile.open( "/".join((os.getcwd(), output)) )
     tar.extractall(path=base_path)
     tar.close()
+    shutil.move(output, base_path/output)
 
     return base_path
 
@@ -51,11 +70,13 @@ def test_load_hack_file(make_test_directory):
 
     filename = make_test_directory / "hackfile"
 
+    # Load hackfile for testing
     hackfile = load_hack_file(filename)
     
+    # Define reference dictionary dimensions
     test_pnt_dict_dims = {'time': 60072, 'az_el': 2, 'ra_dec': 2}
     test_data_dims = {'time': 2401, 'lm': 2, 'chan': 64, 'pol': 4}
-
+    
     assert hackfile['pnt_dict'][0].dims == test_pnt_dict_dims, "load_hack_file::Pointing dict dimensions do not match."
     assert hackfile[0][2][19].dims == test_data_dims, "load_hack_file::Data dimensions do not match."
 
@@ -68,11 +89,15 @@ def test_load_pnt_dict(make_test_directory):
 
     filename = make_test_directory / "test.pnt.dict"
 
+    # Load pointing dictionary 
     pnt_dict = _load_pnt_dict(filename)
     
+    # Define reference pointing dictionary dimensions
     test_pnt_dict_dims = {'time': 60072, 'az_el': 2, 'ra_dec': 2}
 
     assert pnt_dict[0].dims == test_pnt_dict_dims, "load_pnt_dict::Pointing dict dimensions do not match."
 
 def test_version():
+    """ Version test
+    """
     assert __version__ == '0.1.0'
