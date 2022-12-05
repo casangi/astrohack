034e95d4594d2d97895294bcd44ef92bf3dcb2af
Joshua Hoskins <jrh4dy@gmail.com>
2022-12-05 10:07:03 -0500

    Remove call to extract_holog in holog function and add parallel/serial options for calling _holog_chunk.

--- src/astrohack/holog.py
+++ src/astrohack/holog.py
@@ -7,9 +7,6 @@ import dask.array as da
 import numpy as np
 import xarray as xr
 
-from astrohack.dio import extract_holog
-
-@dask.delayed
 def _holog_chunk(ant, hack_file):
         """_summary_
 
@@ -25,26 +22,14 @@ def _holog_chunk(ant, hack_file):
         )
         
 
-def holog(ms_name, hack_name, holog_obs_description, data_col, subscan_intent, parallel):
+def holog(hack_name, parallel=True):
         """_summary_
 
         Args:
-            ms_name (_type_): _description_
             hack_name (_type_): _description_
-            holog_obs_description (_type_): _description_
-            data_col (_type_): _description_
-            subscan_intent (_type_): _description_
-            parallel (_type_): _description_
+            parallel (bool, optional): _description_. Defaults to True.
         """
        
-        extract_holog(
-                ms_name=ms_name,
-                hack_name=hack_name,
-                holog_obs_dict=holog_obs_description,
-                data_col=data_col,
-                subscan_intent=subscan_intent,
-                parallel=parallel
-        )
 
         ant_list = os.listdir('/'.join((hack_name, 'pnt.dict/')))
         ant_list = list(map(int, ant_list))
@@ -52,9 +37,15 @@ def holog(ms_name, hack_name, holog_obs_description, data_col, subscan_intent, p
         delayed_list = []
 
         for ant in ant_list:
-                delayed_list.append(_holog_chunk(ant, hack_name))
+                if parallel:
+                        delayed_list.append(
+                                dask.delayed(_holog_chunk(ant, hack_name))
+                        )
+                else:
+                        _holog_chunk(ant, hack_name)
 
-        dask.compute(delayed_list)
+        if parallel: 
+                dask.compute(delayed_list)
 
 
 
