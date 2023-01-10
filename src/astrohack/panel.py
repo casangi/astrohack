45138aa4db809650fe4778ffc3b4079434c6f02a
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-01-10 09:50:43 -0700

    Added docstrings to AntennaSurface class

--- src/astrohack/panel.py
+++ src/astrohack/panel.py
@@ -546,12 +546,18 @@ class RingPanel:
 
 
 class AntennaSurface:
-    # Describes the antenna surface properties, as well as being
-    # capable of computing the gains and rms over the surface.
-    # Heavily dependent on telescope architecture, Panel geometry to
-    # be created by the specific _init_tel routine
 
     def __init__(self, amp, dev, telescope, cutoff=0.21, pkind=None):
+        """
+        Antenna Surface description capable of computing RMS, Gains, and fitting the surface to obtain screw adjustments
+        Args:
+            amp: Amplitude aperture image (AIPS FITS file)
+            dev: Physical deviation aperture image (AIPS FITS file)
+            telescope: currently supported: ["VLA","VLBA"]
+            cutoff: fractional cutoff on the amplitude image to exclude regions with weak amplitude from the panel
+            surface fitting
+            pkind: Kind of panel surface fitting, if is None defaults to telescope default
+        """
         # Initializes antenna surface parameters    
         self.ampfile = amp
         self.devfile = dev
@@ -590,6 +596,9 @@ class AntennaSurface:
             self.compile_panel_points = self._compile_panel_points_ringed
 
     def _get_aips_headpars(self):
+        """
+        Fetches AIPS specific metadata from FITS headers
+        """
         for line in self.devhead["HISTORY"]:
             wrds = line.split()
             if wrds[1] == "Visibilities":
@@ -602,6 +611,9 @@ class AntennaSurface:
                 self.oulim = abs(float(wrds[-2]))
 
     def _read_images(self):
+        """
+        Reads amplitude and deviation images and initializes the X and Y axes
+        """
         self.amphead, self.amp = read_fits(self.ampfile)
         self.devhead, self.dev = read_fits(self.devfile)
         self.dev *= 1000
@@ -616,6 +628,10 @@ class AntennaSurface:
         return
 
     def _build_ring_mask(self):
+        """
+        Builds the mask on regions to be included in panel surface masks, specific to circular antennas as there is an
+        outer and inner limit to the mask based on the antenna's inner receiver hole and outer edge
+        """
         self.mask = np.where(self.amp < self.cut, False, True)
         self.mask = np.where(self.rad > self.inlim, self.mask, False)
         self.mask = np.where(self.rad < self.oulim, self.mask, False)
@@ -623,7 +639,9 @@ class AntennaSurface:
 
     # Other known telescopes should be included here, ALMA, ngVLA
     def _init_vla(self):
-        # Initializes surfaces according to VLA antenna parameters
+        """
+        Initializes object according to parameters specific to VLA panel distribution
+        """
         self.panelkind = 'flexible'
         self.telescope = "VLA"
         self.diam = 25.0  # meters
@@ -637,7 +655,9 @@ class AntennaSurface:
         self.oulim = 12.0
 
     def _init_vlba(self):
-        # Initializes surfaces according to VLBA antenna parameters
+        """
+        Initializes object according to parameters specific to VLBA panel distribution
+        """
         self.panelkind = "flexible"
         self.telescope = "VLBA"
         self.diam = 25.0  # meters
@@ -651,6 +671,9 @@ class AntennaSurface:
         self.oulim = 12.0
 
     def _build_polar(self):
+        """
+        Build polar coordinate grid, specific for circular antennas with panels arranged in rings
+        """
         self.rad = np.zeros([self.npix, self.npix])
         self.phi = np.zeros([self.npix, self.npix])
         for iy in range(self.npix):
@@ -663,6 +686,9 @@ class AntennaSurface:
                     self.phi[ix, iy] += 2 * np.pi
 
     def _build_ring_panels(self):
+        """
+        Build list of panels, specific for circular antennas with panels arranged in rings
+        """
         self.panels = []
         for iring in range(self.nrings):
             angle = 2.0 * np.pi / self.npanel[iring]
@@ -673,6 +699,10 @@ class AntennaSurface:
         return
 
     def _compile_panel_points_ringed(self):
+        """
+        Loops through the points in the antenna surface and checks to which panels it belongs,
+        specific for circular antennas with panels arranged in rings
+        """
         for iy in range(self.npix):
             yc = self.yaxis.idx_to_coor(iy + 0.5)
             for ix in range(self.npix):
@@ -685,6 +715,16 @@ class AntennaSurface:
                             panel.add_point([xc, yc, ix, iy, self.dev[ix, iy]])
 
     def _fetch_panel_ringed(self, ring, panel):
+        """
+        Fetch a panel object from the panel list using its ring and panel numbers,
+        specific for circular antennas with panels arranged in rings
+        Args:
+            ring: Ring number
+            panel: Panel number
+
+        Returns:
+        Panel object
+        """
         if ring == 1:
             ipanel = panel - 1
         else:
@@ -692,6 +732,11 @@ class AntennaSurface:
         return self.panels[ipanel]
 
     def gains(self):
+        """
+        Computes antenna gains in decibels before and after panel surface fitting
+        Returns:
+        Gains before panel fitting OR Gains before and after panel fitting
+        """
         self.ingains = self._gains_array(self.dev)
         if self.resi is None:
             return self.ingains
@@ -700,6 +745,14 @@ class AntennaSurface:
             return self.ingains, self.ougains
 
     def _gains_array(self, arr):
+        """
+        Worker for gains method, works with the actual arrays to compute the gains
+        Args:
+            arr: Deviation image over which to compute the gains
+
+        Returns:
+        Actual and theoretical gains
+        """
         # Compute the actual and theoretical gains for the current
         # antenna surface. What is the unit for the wavelength? mm 
         forpi = 4.0 * np.pi
@@ -733,7 +786,11 @@ class AntennaSurface:
         return gain, thgain
 
     def get_rms(self):
-        # Compute the RMS of the antenna surface
+        """
+        Computes antenna surface RMS before and after panel surface fitting
+        Returns:
+        RMS before panel fitting OR RMS before and after panel fitting
+        """
         self.inrms = np.sqrt(np.mean(self.dev[self.mask] ** 2))
         if self.resi is None:
             return self.inrms
@@ -742,11 +799,17 @@ class AntennaSurface:
             return self.inrms, self.ourms
 
     def fit_surface(self):
+        """
+        Loops over the panels to fit the panel surfaces
+        """
         for panel in self.panels:
             panel.solve()
         self.solved = True
 
     def correct_surface(self):
+        """
+        Apply corrections determined by the panel surface fitting methods to the antenna surface
+        """
         if not self.solved:
             raise Exception("Panels must be fitted before atempting a correction")
         self.corr = np.where(self.mask, 0, np.nan)
@@ -760,10 +823,20 @@ class AntennaSurface:
                 self.corr[ix, iy] = -panel.corr[ipnt]
 
     def print_misc(self):
+        """
+        Print miscelaneous information on the panels in the antenna surface
+        """
         for panel in self.panels:
             panel.print_misc()
 
     def plot_surface(self, filename=None, mask=False, screws=False):
+        """
+        Do plots of the antenna surface
+        Args:
+            filename: Save plot to a file rather than displaying it with matplotlib widgets
+            mask: Display mask and amplitudes rather than deviation images
+            screws: Display the screws on the panels
+        """
         vmin, vmax = np.nanmin(self.dev), np.nanmax(self.dev)
         rms = self.get_rms()
         if mask:
@@ -797,6 +870,19 @@ class AntennaSurface:
 
     def _plot_surface(self, data, title, fig, ax, vmin, vmax, screws=False, mask=False,
                       unit='mm'):
+        """
+        Does the plotting of a data array in a figure's subplot
+        Args:
+            data: The array to be plotted
+            title: Title of the subplot
+            fig: Global figure containing the subplots
+            ax: matplotlib axes instance describing the subplot
+            vmin: minimum to the color scale
+            vmax: maximum to the color scale
+            screws: Display screws
+            mask: do not add colorbar if plotting a mask
+            unit: Unit of the data in the color scale
+        """
         ax.set_title(title)
         # set the limits of the plot to the limits of the data
         xmin = self.xaxis.idx_to_coor(-0.5)
@@ -815,6 +901,11 @@ class AntennaSurface:
             panel.plot(ax, screws=screws)
 
     def export_corrected(self, filename):
+        """
+        Export corrected surface to a FITS file
+        Args:
+            filename: Output FITS file name/path
+        """
         if self.resi is None:
             raise Exception("Cannot export corrected surface")
         hdu = fits.PrimaryHDU(self.resi)
@@ -824,6 +915,12 @@ class AntennaSurface:
         return
 
     def export_screw_adjustments(self, filename, unit='mm'):
+        """
+        Export screw adjustments for all panels onto an ASCII file
+        Args:
+            filename: ASCII file name/path
+            unit: unit for panel screw adjustments ['mm','miliinches']
+        """
         spc = ' '
         outfile = 'Screw adjustments for {0:s} {1:s} antenna\n'.format(
             self.telescope, self.amphead['telescop'])
