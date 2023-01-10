75b6ea05de00140a37a6dc8be6ccf22cd01782f1
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-01-09 17:14:32 -0700

    Added docstrings to classes RingPanel and LinearAxis

--- src/astrohack/panel.py
+++ src/astrohack/panel.py
@@ -9,16 +9,39 @@ lnbr = '\n'
 
 # static methods not linked to any specific class
 def gauss_numpy(system, vector):
+    """
+    Gauss elimination solving of a system using numpy
+    Args:
+        system: System matrix to be solved
+        vector: Vector that represents the right hand side of the system
+
+    Returns:
+    The solved system
+    """
     inverse = np.linalg.inv(system)
     return np.dot(inverse, vector)
 
 
-def convert_to_db(val):
-    # Convert to decibels
+def convert_to_db(val: float):
+    """
+    Converts a float value to decibels
+    Args:
+        val (float): Value to be converted to decibels
+    Returns:
+        Value in decibels
+    """
     return 10. * np.log10(val)
 
 
 def read_fits(filename):
+    """
+    Reads a FITS file and do sanity checks on its dimensionality
+    Args:
+        filename: a string containing the FITS file name/path
+
+    Returns:
+    The FITS header and the associated data array
+    """
     hdul = fits.open(filename)
     head = hdul[0].header
     if head['NAXIS'] != 2:
@@ -31,11 +54,7 @@ def read_fits(filename):
     if head['NAXIS1'] != head['NAXIS2']:
         raise Exception(filename + " image is not square")
 
-    if "AIPS" in hdul[0].header["ORIGIN"]:
-        # AIPS data is in meters
-        data = hdul[0].data[0, 0, :, :]
-    else:
-        data = hdul[0].data[0, 0, :, :]
+    data = hdul[0].data[0, 0, :, :]
     hdul.close()
     return head, data
 
@@ -44,15 +63,38 @@ class LinearAxis:
     # According to JWS this class is superseded by xarray, which
     # should be used instead
     def __init__(self, n, ref, val, inc):
+        """
+        Args:
+            n:   Axis size
+            ref: Refence element in the axis
+            val: Value at ref
+            inc: Increment between axis elements
+        """
         self.n = n
         self.ref = ref
         self.val = val
         self.inc = inc
 
     def idx_to_coor(self, idx):
+        """
+        Converts from an index position to a coordinate
+        Args:
+            idx: index position
+
+        Returns:
+        Coordinate at idx
+        """
         return (idx - self.ref) * self.inc + self.val
 
     def coor_to_idx(self, coor):
+        """
+        Converts from a coordinate to an index
+        Args:
+            coor: coordinate position
+
+        Returns:
+        index at coor
+        """
         return (coor - self.val) / self.inc + self.ref
 
 
@@ -61,7 +103,17 @@ class RingPanel:
     # rings on the Antenna surface
 
     def __init__(self, kind, angle, iring, ipanel, inrad, ourad):
-        # Panel initialization
+        """
+        Initializes a panel that is a section of a ring in a circular antenna
+        Args:
+            kind: What kind of surface to be used in fitting ["flexible", "rigid", "single", "xyparaboloid",
+            "thetaparaboloid", "fixedtheta"]
+            angle: Azimuthal span of the panel
+            iring: Which ring is the panel in
+            ipanel: Panel number clockwise from top
+            inrad: Radius at panel inner side
+            ourad: Radius at panel outer side
+        """
         self.kind = kind
         self.ipanel = ipanel + 1
         self.iring = iring + 1
@@ -108,21 +160,30 @@ class RingPanel:
             self.solve = self._solve_single
             self.corr_point = self._corr_point_single
         elif self.kind == "xyparaboloid":
-            self.solve = self._solve_flexi_scipy
+            self.solve = self._solve_scipy
             self.corr_point = self._corr_point_flexi_scipy
             self._paraboloid = self._xyaxes_paraboloid
         elif self.kind == "thetaparaboloid":
-            self.solve = self._solve_flexi_scipy
+            self.solve = self._solve_scipy
             self.corr_point = self._corr_point_flexi_scipy
             self._paraboloid = self._rotated_paraboloid
         elif self.kind == "fixedtheta":
-            self.solve = self._solve_flexi_scipy
+            self.solve = self._solve_scipy
             self.corr_point = self._corr_point_flexi_scipy
             self._paraboloid = self._fixed_paraboloid
         else:
             raise Exception("Unknown panel kind: ", self.kind)
 
     def is_inside(self, rad, phi):
+        """
+        Check if a point is inside a panel using polar coordinates
+        Args:
+            rad: radius of the point
+            phi: angle of the point in polar coordinates
+
+        Returns:
+        True if point is inside the panel, False otherwise
+        """
         # Simple test of polar coordinates to check that a point is
         # inside this panel
         angle = self.theta1 <= phi <= self.theta2
@@ -130,12 +191,18 @@ class RingPanel:
         return angle and radius
 
     def add_point(self, value):
+        """
+        Add a point to the panel's list of point to be fitted
+        Args:
+            value: tuple/list containing point description [xcoor,ycoor,xidx,yidx,value]
+        """
         self.values.append(value)
         self.nsamp += 1
 
     def _solve_flexi(self):
-        # Solve panel adjustments for flexible VLA style panels by
-        # constructing a system of 4 linear equations
+        """
+        Fit panel surface using AIPS gauss elimination model for flexible panels
+        """
         syssize = 4
         if self.nsamp < syssize:
             # In this case the matrix will always be singular as the
@@ -177,9 +244,13 @@ class RingPanel:
 
         self.par = gauss_numpy(system, vector)
         self.solved = True
-        return
 
-    def _solve_flexi_scipy(self, verbose=False):
+    def _solve_scipy(self, verbose=False):
+        """
+        Fit ponel surface by using arbitrary models using scipy surface fitting engine
+        Args:
+            verbose: Increase verbosity in the fitting process
+        """
         devia = np.ndarray([self.nsamp])
         coords = np.ndarray([2, self.nsamp])
         for i in range(self.nsamp):
@@ -213,12 +284,19 @@ class RingPanel:
                 break
 
     def _fixed_paraboloid(self, coords, ucurv, vcurv, zoff):
-        # Same as the rotated paraboloid, but theta is the panel center
-        # This assumes that the center of the paraboloid is the center
-        # of the panel, is this reasonable?
-        # Also, this function can produce degeneracies, due to the fact
-        # that there are multiple combinations of theta u curvature and
-        # v curvature that produce the same paraboloid
+        """
+        Surface model to be used in fitting with scipy
+        Same as the rotated paraboloid below, but theta is the panel center angle
+        Assumes that the center of the paraboloid is the center of the panel
+        Args:
+            coords: [x,y] coordinate pair for point
+            ucurv: curvature in projected u direction
+            vcurv: curvature in projected v direction
+            zoff:  Z offset of the paraboloid
+
+        Returns:
+        Paraboloid value at X and Y
+        """
         x, y = coords
         xc, yc = self.center
         u = (x - xc) * np.cos(self.zeta) + (y - yc) * np.sin(self.zeta)
@@ -226,26 +304,47 @@ class RingPanel:
         return -((u / ucurv) ** 2 + (v / vcurv) ** 2) + zoff
 
     def _rotated_paraboloid(self, coords, ucurv, vcurv, zoff, theta):
-        # This assumes that the center of the paraboloid is the center
-        # of the panel, is this reasonable?
-        # Also, this function can produce degeneracies, due to the fact
-        # that there are multiple combinations of theta u curvature and
-        # v curvature that produce the same paraboloid
+        """
+        Surface model to be used in fitting with scipy
+        Assumes that the center of the paraboloid is the center of the panel
+        This model is degenerate in the combinations of theta, ucurv and vcurv
+        Args:
+            coords: [x,y] coordinate pair for point
+            ucurv: curvature in projected u direction
+            vcurv: curvature in projected v direction
+            zoff:  Z offset of the paraboloid
+            theta: Angle between x,y and u,v coordinate systems
+
+        Returns:
+        Paraboloid value at X and Y
+        """
         x, y = coords
         xc, yc = self.center
         u = (x - xc) * np.cos(theta) + (y - yc) * np.sin(theta)
         v = (x - xc) * np.sin(theta) + (y - yc) * np.cos(theta)
         return -((u / ucurv) ** 2 + (v / vcurv) ** 2) + zoff
 
-    def _xyaxes_paraboloid(self, coords, a, b, c):
-        # This assumes that the center of the paraboloid is the center
-        # of the panel, is this reasonable?
+    def _xyaxes_paraboloid(self, coords, xcurv, ycurv, zoff):
+        """
+        Surface model to be used in fitting with scipy
+        Assumes that the center of the paraboloid is the center of the panel
+        In this model the panel can only bend in the x and y directions
+        Args:
+            coords: [x,y] coordinate pair for point
+            xcurv: curvature in x direction
+            ycurv: curvature in y direction
+            zoff:  Z offset of the paraboloid
+
+        Returns:
+        Paraboloid value at X and Y
+        """
         x, y = coords
-        return -(((x - self.center[0]) / a) ** 2 + ((y - self.center[1]) / b) ** 2) + c
+        return -(((x - self.center[0]) / xcurv) ** 2 + ((y - self.center[1]) / ycurv) ** 2) + zoff
 
     def _solve_rigid(self):
-        # Solve panel adjustments for rigid tilt and shift only panels by
-        # constructing a system of 3 linear equations
+        """
+        Fit panel surface using AIPS gauss elimination model for rigid panels
+        """
         syssize = 3
         if self.nsamp < syssize:
             # In this case the matrix will always be singular as the
@@ -273,6 +372,9 @@ class RingPanel:
         return
 
     def _solve_single(self):
+        """
+        Fit panel surface as a simple mean of its points Z deviation
+        """
         if self.nsamp > 0:
             # Solve panel adjustments for rigid vertical shift only panels
             self.par = np.zeros(1)
@@ -291,6 +393,9 @@ class RingPanel:
         return
 
     def get_corrections(self):
+        """
+        Store corrections for the fitted panel points
+        """
         if not self.solved:
             raise Exception("Cannot correct a panel that is not solved")
         self.corr = np.ndarray([len(self.values)])
@@ -298,9 +403,17 @@ class RingPanel:
         for val in self.values:
             self.corr[icorr] = self.corr_point(val[0], val[1])
             icorr += 1
-        return
 
     def _corr_point_flexi(self, xcoor, ycoor):
+        """
+        Computes fitted value for point [xcoor, ycoor] using AIPS gauss elimination model for flexible panels
+        Args:
+            xcoor: X coordinate of point
+            ycoor: Y coordinate of point
+
+        Returns:
+        Fitted value at xcoor,ycoor
+        """
         coef = np.ndarray([4])
         corrval = 0
         fac = self.bmp[0] + ycoor * (self.tmp[0] - self.bmp[0]) / self.tmp[1]
@@ -313,16 +426,52 @@ class RingPanel:
         return corrval
 
     def _corr_point_flexi_scipy(self, xcoor, ycoor):
+        """
+        Computes fitted value for point [xcoor, ycoor] using the scipy models
+        Args:
+            xcoor: X coordinate of point
+            ycoor: Y coordinate of point
+
+        Returns:
+        Fitted value at xcoor,ycoor
+        """
         corrval = self._paraboloid([xcoor, ycoor], *self.par)
         return corrval
 
     def _corr_point_rigid(self, xcoor, ycoor):
+        """
+        Computes fitted value for point [xcoor, ycoor] using AIPS gauss elimination model for rigid panels
+        Args:
+            xcoor: X coordinate of point
+            ycoor: Y coordinate of point
+
+        Returns:
+        Fitted value at xcoor,ycoor
+        """
         return xcoor * self.par[0] + ycoor * self.par[1] + self.par[2]
 
     def _corr_point_single(self, xcoor, ycoor):
+        """
+        Computes fitted value for point [xcoor, ycoor] using AIPS shift only panels
+        Args:
+            xcoor: X coordinate of point
+            ycoor: Y coordinate of point
+
+        Returns:
+        Fitted value at xcoor,ycoor
+        """
         return self.par[0]
 
     def export_adjustments(self, unit='mm', screen=False):
+        """
+        Exports panel screw adjustments to a string
+        Args:
+            unit: Unit for screw adjustments ['mm','miliinches']
+            screen: display values in terminal
+
+        Returns:
+        String with screw adjustments for this panel
+        """
         if unit == 'mm':
             fac = 1.0
         elif unit == 'miliinches':
@@ -338,6 +487,11 @@ class RingPanel:
         return string
 
     def print_misc(self, verbose=False):
+        """
+        Print miscelaneous information about the panel to the terminal
+        Args:
+            verbose: Include more information in print
+        """
         print("########################################")
         print("{0:20s}={1:8d}".format("ipanel", self.ipanel))
         print("{0:20s}={1:8s}".format("kind", " " + self.kind))
@@ -358,6 +512,13 @@ class RingPanel:
         print()
 
     def plot(self, ax, screws=False):
+        """
+        Plot panel outline to ax
+        Args:
+            ax: matplotlib axes instance
+            screws: Display screws in plot
+
+        """
         lw = 0.5
         msize = 2
         x1 = self.inrad * np.sin(self.theta1)
@@ -377,6 +538,7 @@ class RingPanel:
                 ax.scatter(screw[0], screw[1], marker=markers[iscrew],
                            lw=lw, s=msize, color=colors[iscrew])
         if self.ipanel == 1:
+            # Draw ring outline with first panel
             inrad = plt.Circle((0, 0), self.inrad, color='black', fill=False, lw=lw)
             ourad = plt.Circle((0, 0), self.ourad, color='black', fill=False, lw=lw)
             ax.add_patch(inrad)
