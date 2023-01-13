6afe4085c224241db7643ea679836ec754210d28
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-01-13 09:44:13 -0700

    PANEL: Linear axis class moved to a new file in the _classes subdirectory

--- src/astrohack/panel.py
+++ src/astrohack/panel.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 from astropy.io import fits
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy import optimize as opt
+from astrohack._classes.linear_axis import LinearAxis
 
 lnbr = "\n"
 
@@ -57,45 +58,6 @@ def read_fits(filename):
     data = hdul[0].data[0, 0, :, :]
     hdul.close()
     return head, data
-
-
-class LinearAxis:
-    # According to JWS this class is superseded by xarray, which
-    # should be used instead
-    def __init__(self, n, ref, val, inc):
-        """
-        Args:
-            n:   Axis size
-            ref: Refence element in the axis
-            val: Value at ref
-            inc: Increment between axis elements
-        """
-        self.n = n
-        self.ref = ref
-        self.val = val
-        self.inc = inc
-
-    def idx_to_coor(self, idx):
-        """
-        Converts from an index position to a coordinate
-        Args:
-            idx: index position
-
-        Returns:
-        Coordinate at idx
-        """
-        return (idx - self.ref) * self.inc + self.val
-
-    def coor_to_idx(self, coor):
-        """
-        Converts from a coordinate to an index
-        Args:
-            coor: coordinate position
-
-        Returns:
-        index at coor
-        """
-        return (coor - self.val) / self.inc + self.ref
 
 
 class RingPanel:
