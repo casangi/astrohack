c19388ffa6b6e3372c8a77c995ab52419bb60cef
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-01-17 08:55:58 -0700

    PANEL: First steps to implement cfg file reading for panel arrangement

--- src/astrohack/_classes/telescope.py
+++ src/astrohack/_classes/telescope.py
@@ -1,5 +1,8 @@
-class Telescope:
+ring_list_keys = ['npanel', 'inrad', 'ourad']
+ring_simple_keys = ['name', 'diam', 'focus', 'nrings', 'inlim', 'oulim']
 
+
+class Telescope:
     def __init__(self, name):
         """
         Initializes antenna surface relevant information based on the telescope name
@@ -12,6 +15,63 @@ class Telescope:
             self._init_vlba()
         else:
             raise Exception("Unknown telescope: " + name)
+        return
+
+    def _read_cfg_file(self, filename):
+        try:
+            cfgfile = open(filename, 'r')
+            ledict = {}
+            for line in cfgfile:
+                wrds = line.split('=')
+                ledict[wrds[0].strip()] = wrds[1]
+            cfgfile.close()
+        except:
+            raise Exception('Badly formatted cfg file')
+
+        print(ledict)
+        print(ledict['ringed'])
+        try:
+            self.ringed = bool(ledict['ringed'])
+        except KeyError:
+            raise Exception('Ringed keyword missing from cfg file')
+        except ValueError:
+            raise Exception('Value for keyword ringed is not boolean')
+
+        if self.ringed:
+            self._init_ringed_telescope(ledict)
+        else:
+            self._init_general_telescope(ledict)
+
+        return
+
+    def _init_ringed_telescope(self, ledict):
+        for key in ring_simple_keys:
+            try:
+                setattr(self, key, float(ledict[key]))
+            except KeyError:
+                raise Exception(key+' keyword missing from cfg file')
+            except ValueError:
+                raise Exception('Cannot convert '+ledict[key]+' to float')
+
+        for key in ring_list_keys:
+            try:
+                wrds = ledict[key].split(',')
+                lelist = []
+                for word in wrds:
+                    lelist.append(float(word))
+                setattr(self, key, lelist)
+            except KeyError:
+                raise Exception(key+' keyword missing from cfg file')
+            except ValueError:
+                raise Exception('Failed to convert values to float for keyword '+key)
+
+        if not self.npanel == len(self.npanel) == len(self.inrad) == self.ourad:
+            raise Exception('Number of panels don\'t match radii or number of panels list sizes')
+
+        return
+
+    def _init_general_telescope(self, ledict):
+        raise Exception("General layout telescopes not yet supported")
         return
 
     # Other known telescopes should be included here, ALMA, ngVLA
