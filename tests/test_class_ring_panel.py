adf7440e60d0f64e7b684798cace0d4196d2a58d
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-01-20 11:23:56 -0700

    PANEL: Unit tests for RingPanel class

--- tests/test_class_ring_panel.py
+++ tests/test_class_ring_panel.py
@@ -4,6 +4,7 @@ from astrohack._classes.ring_panel import RingPanel, _gauss_elimination_numpy
 import numpy as np
 
 kinds = ['single', 'rigid', 'xyparaboloid', 'thetaparaboloid', 'fixedtheta']
+mm2mi = 1000 / 25.4
 
 
 class TestRingPanel:
@@ -11,6 +12,8 @@ class TestRingPanel:
     ourad = 3.0
     angle = np.pi/2
     position = 1
+    deviation = 2.0
+    point = [2.5, -2.5, 1, 1, deviation]
     fixedthetapan = RingPanel('fixedtheta', angle, position, position, inrad, ourad)
 
     def test_gauss_elimination_numpy(self):
@@ -33,8 +36,32 @@ class TestRingPanel:
         assert not self.fixedthetapan.is_inside((self.inrad + self.ourad) / 2, 3.5 * self.angle)
 
     def test_add_point(self):
-        point = [2.5, -2.5, 1, 1, 2.0]
-        self.fixedthetapan.add_point(point)
+        self.fixedthetapan.add_point(self.point)
         assert self.fixedthetapan.nsamp == 1
-        assert self.fixedthetapan.values[0] == point
+        assert self.fixedthetapan.values[0] == self.point
         assert len(self.fixedthetapan.values) == 1
+
+    def test_solve(self):
+        npoints = 200
+        for i in range(npoints):
+            self.fixedthetapan.add_point(self.point)
+        self.fixedthetapan.solve()
+        assert self.fixedthetapan.solved
+        assert self.fixedthetapan.par[0] > 1e2
+        assert self.fixedthetapan.par[1] > 1e2
+        assert abs(self.fixedthetapan.par[2] - self.deviation) < 1e-3
+
+    def test_get_correction(self):
+        self.fixedthetapan.get_corrections()
+        assert len(self.fixedthetapan.corr) == self.fixedthetapan.nsamp
+        for isamp in range(self.fixedthetapan.nsamp):
+            assert abs(self.fixedthetapan.corr[isamp] - self.deviation) < 1e-3
+
+    def test_export_adjustments(self):
+        mmscrews = self.fixedthetapan.export_adjustments().split()[2:]
+        for screw in mmscrews:
+            assert abs(float(screw) - self.deviation) < 1e-3
+        miscrews = self.fixedthetapan.export_adjustments(unit='miliinches').split()[2:]
+        for screw in miscrews:
+            print(screw, mm2mi*self.deviation)
+            assert abs(float(screw) - mm2mi*self.deviation) < 1e-2
