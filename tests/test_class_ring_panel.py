21010d7ce6ee51fda4351f43e38339679642b5a1
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-01-20 14:10:36 -0700

    PANEL: Added documentation for the RingPanel class unit tests

--- tests/test_class_ring_panel.py
+++ tests/test_class_ring_panel.py
@@ -3,7 +3,6 @@ import pytest
 from astrohack._classes.ring_panel import RingPanel, _gauss_elimination_numpy
 import numpy as np
 
-kinds = ['single', 'rigid', 'xyparaboloid', 'thetaparaboloid', 'fixedtheta']
 mm2mi = 1000 / 25.4
 
 
@@ -17,6 +16,9 @@ class TestRingPanel:
     fixedthetapan = RingPanel('fixedtheta', angle, position, position, inrad, ourad)
 
     def test_gauss_elimination_numpy(self):
+        """
+        Tests the gaussian elimination routine by using an identity matrix
+        """
         size = 3
         identity = np.identity(size)
         vector = np.arange(size)
@@ -24,6 +26,9 @@ class TestRingPanel:
             assert _gauss_elimination_numpy(identity, vector)[pos] == vector[pos]
 
     def test_init(self):
+        """
+        Tests the correct initialization of a RingPanel object, not all parameters tested
+        """
         assert self.fixedthetapan.theta1 == self.angle
         assert self.fixedthetapan.theta2 == 2 * self.angle
         assert self.fixedthetapan.zeta == 1.5 * self.angle
@@ -32,16 +37,25 @@ class TestRingPanel:
             panel = RingPanel('xxx', self.angle, self.position, self.position, self.inrad, self.ourad)
 
     def test_is_inside(self):
+        """
+        Test over the is_inside test for a point
+        """
         assert self.fixedthetapan.is_inside((self.inrad + self.ourad) / 2, 1.5 * self.angle)
         assert not self.fixedthetapan.is_inside((self.inrad + self.ourad) / 2, 3.5 * self.angle)
 
     def test_add_point(self):
+        """
+        Tests the addition of a point to the panel list of points
+        """
         self.fixedthetapan.add_point(self.point)
         assert self.fixedthetapan.nsamp == 1
         assert self.fixedthetapan.values[0] == self.point
         assert len(self.fixedthetapan.values) == 1
 
     def test_solve(self):
+        """
+        Tests the solving of the panel surface but using only the scipy fitting of the fixedtheta version
+        """
         npoints = 200
         for i in range(npoints):
             self.fixedthetapan.add_point(self.point)
@@ -52,12 +66,18 @@ class TestRingPanel:
         assert abs(self.fixedthetapan.par[2] - self.deviation) < 1e-3
 
     def test_get_correction(self):
+        """
+        Tests that corrections are what are expected based on the input data
+        """
         self.fixedthetapan.get_corrections()
         assert len(self.fixedthetapan.corr) == self.fixedthetapan.nsamp
         for isamp in range(self.fixedthetapan.nsamp):
             assert abs(self.fixedthetapan.corr[isamp] - self.deviation) < 1e-3
 
     def test_export_adjustments(self):
+        """
+        Tests that panel adjustments are within what is expected from input data
+        """
         mmscrews = self.fixedthetapan.export_adjustments().split()[2:]
         for screw in mmscrews:
             assert abs(float(screw) - self.deviation) < 1e-3
