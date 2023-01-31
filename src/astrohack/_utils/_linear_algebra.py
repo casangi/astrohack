d4f2050eb8fa06de671c5e758e01e347a967eebb
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-01-31 14:01:56 -0700

    Phase-fitting: Added AIPS least_squares fit function rewritten using numpy

--- src/astrohack/_utils/_linear_algebra.py
+++ src/astrohack/_utils/_linear_algebra.py
@@ -13,3 +13,26 @@ def _gauss_elimination_numpy(system, vector):
     """
     inverse = np.linalg.inv(system)
     return np.dot(inverse, vector)
+
+
+def _least_squares_fit(system, vector):
+    """
+    Least squares fitting of a system of linear equations
+    Args:
+        system: System matrix to be solved
+        vector: Vector that represents the right hand side of the system
+
+    Returns:
+    The solved system, the variances of the system solution and the sum of the residuals
+    """
+    if len(system.shape) != 2:
+        raise Exception('System must have 2 dimensions')
+    if system.shape[0] != system.shape[1]:
+        raise Exception('System must be a square matrix')
+    rank = system.shape[0]
+    fit = np.linalg.lstsq(system, vector)
+    result = fit[0]
+    residuals = fit[1]
+    covar = np.matrix(np.dot(system.T, system)).I
+    variances = np.dot(covar, np.identity(rank))
+    return result, variances, residuals
