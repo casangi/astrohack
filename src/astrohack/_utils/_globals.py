fd804b964d614f3cbd569a8d67aa4e60e65e18c7
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-02-06 13:17:09 -0700

    Panel: Added skeleton of global constants

---
+++ src/astrohack/_utils/_globals.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+# Physical constants
+clight = 299792458.0
+
+# Unit conversion
+mm2m = 1/1000.
+m2mm = 1000.
+m2mils = 1e6 / 2.54
+mils2m = 1. / m2mils
+
+
+# Trigonometric unit conversions
+rad2deg = 180. / np.pi
+deg2rad = np.pi / 180.
+
