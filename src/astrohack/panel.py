2a23e441910b08d6ba4e65632025fa52855810b2
Joshua Hoskins <jrh4dy@gmail.com>
2023-02-10 11:27:24 -0500

    Clean up notebook a bit and change soem formatting. Also increased radius of mask in holog.

--- src/astrohack/panel.py
+++ src/astrohack/panel.py
@@ -1,9 +1,10 @@
+import os
+import dask
+import xarray as xr
+
 from astrohack._classes.antenna_surface import AntennaSurface
 from astrohack._classes.telescope import Telescope
 from astrohack._utils._io import _load_image_xds
-import os
-import xarray as xr
-import dask
 
 
 def panel(holog_image, outfile, aipsdata=False, telescope=None, cutoff=None, panel_kind=None, basename=None, unit='mm',
@@ -25,34 +26,44 @@ def panel(holog_image, outfile, aipsdata=False, telescope=None, cutoff=None, pan
         save_phase: Save plot of phases to a png file
         parallel: Run chunks of processing in parallel
     """
+
     outfile += '.panel.zarr'
-    panel_chunk_params = {'holog_image': holog_image,
-                          'unit': unit,
-                          'panel_kind': panel_kind,
-                          'cutoff': cutoff,
-                          'save_mask': save_mask,
-                          'save_deviations': save_deviations,
-                          'save_phase': save_phase,
-                          'telescope': telescope,
-                          'basename': basename,
-                          'outfile': outfile
-                          }
+
+    panel_chunk_params = {
+        'holog_image': holog_image,
+        'unit': unit,
+        'panel_kind': panel_kind,
+        'cutoff': cutoff,
+        'save_mask': save_mask,
+        'save_deviations': save_deviations,
+        'save_phase': save_phase,
+        'telescope': telescope,
+        'basename': basename,
+        'outfile': outfile
+    }
+    
     os.makedirs(name=outfile,  exist_ok=True)
+
     if aipsdata:
         if telescope is None:
             raise Exception('For AIPS data a telescope must be specified')
+        
         if basename is None:
             raise Exception('For AIPS data a basename must be specified')
+        
         panel_chunk_params['origin'] = 'AIPS'
         _panel_chunk(panel_chunk_params)
+
     else:
         panel_chunk_params['origin'] = 'astrohack'
         delayed_list = []
         fullname = holog_image+'.image.zarr'
         antennae = os.listdir(fullname)
+        
         for antenna in antennae:
             panel_chunk_params['antenna'] = antenna
             ddis = os.listdir(fullname+'/'+antenna)
+        
             for ddi in ddis:
                 panel_chunk_params['ddi'] = ddi
                 if parallel:
@@ -69,21 +80,27 @@ def _panel_chunk(panel_chunk_params):
     Args:
         panel_chunk_params: dictionary of inputs
     """
+
     if panel_chunk_params['origin'] == 'AIPS':
         telescope = Telescope(panel_chunk_params['telescope'])
         inputxds = xr.open_zarr(panel_chunk_params['holog_image'])
         suffix = ''
+
     else:
         inputxds = _load_image_xds(panel_chunk_params['holog_image'],
                                    panel_chunk_params['antenna'],
                                    panel_chunk_params['ddi'])
+        
         inputxds.attrs['AIPS'] = False
         inputxds.attrs['antenna_name'] = panel_chunk_params['antenna']
+
         if panel_chunk_params['telescope'] is None:
             tname = inputxds.attrs['telescope_name']+'_'+inputxds.attrs['ant_name'][0:2]
             telescope = Telescope(tname)
+
         else:
             telescope = Telescope(panel_chunk_params['telescope'])
+            
         suffix = '_' + inputxds.attrs['ant_name'] + '/' + panel_chunk_params['ddi']
 
     surface = AntennaSurface(inputxds, telescope, panel_chunk_params['cutoff'], panel_chunk_params['panel_kind'])
@@ -93,16 +110,22 @@ def _panel_chunk(panel_chunk_params):
 
     if panel_chunk_params['basename'] is None:
         basename = panel_chunk_params['outfile'] + '/' + telescope.name + suffix
+
     else:
         basename = panel_chunk_params['outfile'] + '/' + panel_chunk_params['basename'] + suffix
+
     os.makedirs(name=basename, exist_ok=True)
+
     basename += "/"
     xds = surface.export_xds()
     xds.to_zarr(basename+'xds.zarr', mode='w')
     surface.export_screw_adjustments(basename + "screws.txt", unit=panel_chunk_params['unit'])
+    
     if panel_chunk_params['save_mask']:
         surface.plot_surface(filename=basename + "mask.png", mask=True, screws=True)
+    
     if panel_chunk_params['save_deviations']:
         surface.plot_surface(filename=basename + "surface.png")
+    
     if panel_chunk_params['save_phase']:
         surface.plot_surface(filename=basename + "phase.png", plotphase=True)
