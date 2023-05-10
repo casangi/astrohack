6dced980b2befe745f0fb3ce28d4d415bfbcec5a
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-05-10 11:09:33 -0600

    metadata improvements: Improved the treatment of Nones

--- src/astrohack/_utils/_io.py
+++ src/astrohack/_utils/_io.py
@@ -333,12 +333,19 @@ def _write_meta_data(origin, file_name, input_dict):
                 'origin': origin}
     for key in input_dict.keys():
         if type(input_dict[key]) == np.ndarray:
-            for item in range(len(input_dict[key])):
-                newkey = f'{key}_{item}'
-                metadata[newkey] = _numpy_to_json(input_dict[key][item])
+            try:
+                for item in range(len(input_dict[key])):
+                    newkey = f'{key}_{item}'
+                    metadata[newkey] = _numpy_to_json(input_dict[key][item])
+            except TypeError:
+                if len(input_dict['grid_size'].shape) == 0:
+                    metadata[key] = "None"
+                else:
+                    metadata[key] = input_dict[key]
+        elif input_dict[key] is None:
+            metadata[key] = "None"
         else:
             metadata[key] = input_dict[key]
-
     try:
         with open(file_name, "w") as json_file:
             json.dump(metadata, json_file)
