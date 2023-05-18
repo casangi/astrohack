1a2fb457e806c2326068bd6222c97ceb8f0ed418
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-05-18 10:56:51 -0600

    to-fits: Added function that factorized the simple loop over ddi and antenna of many DIO functions

--- src/astrohack/_utils/_tools.py
+++ src/astrohack/_utils/_tools.py
@@ -1,6 +1,7 @@
 import os
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
+import dask
 from astrohack._utils._logger._astrohack_logger import _get_astrohack_logger
 import json
 
@@ -118,7 +119,7 @@ def _print_holog_obs_dict(holog_obj):
     print("{close_bracket}".format(close_bracket=CLOSE_DICT))
 
 
-def _parm_to_list(parm, path):
+def _parm_to_list(parm, path, prefix):
     """
     Transforms a string parameter to a list if parameter is all or a single string
     Args:
@@ -129,7 +130,11 @@ def _parm_to_list(parm, path):
 
     """
     if parm == 'all':
-        oulist = os.listdir(path)
+        tmplist = os.listdir(path)
+        oulist = []
+        for item in tmplist:
+            if item.find(prefix) == 0:
+                oulist.append(item)
     elif isinstance(parm, str):
         oulist = [parm]
     else:
@@ -262,3 +267,52 @@ def _bool_to_string(flag):
         return 'yes'
     else:
         return 'no'
+
+
+def _factorized_antenna_ddi_loop(function_name, chunk_function, parm_dict, parallel):
+    """
+    Simple routine to factorize the possibily parallel loop of processing
+    Args:
+        function_name: The caller function
+        chunk_function: The chunk function to be passed to dask
+        parm_dict: The parameter dictionary to be used by the chunk function
+        parallel: Should the loop be executed in parallel?
+
+    Returns: How many times the loop has been executed
+
+    """
+    logger = _get_astrohack_logger()
+    delayed_list = []
+    antennae = _parm_to_list(parm_dict['ant_name'], parm_dict['filename'], 'ant')
+    if len(antennae) == 0:
+        logger.warning(f'[{function_name}]: Antenna list is empty for antenna selection: {parm_dict["ant_name"]}')
+    count = 0
+    for antenna in antennae:
+        parm_dict['this_antenna'] = antenna
+        path = f'{parm_dict["filename"]}/{antenna}'
+        if os.path.isdir(path):
+            ddis = _parm_to_list(parm_dict['ddi'], f'{parm_dict["filename"]}/{antenna}', 'ddi')
+            if len(ddis) == 0:
+                logger.warning(
+                    f'[{function_name}]: DDI list for antenna {antenna} is empty for ddi selection: {parm_dict["ddi"]}')
+            for ddi in ddis:
+                parm_dict['this_ddi'] = ddi
+                path = f'{parm_dict["filename"]}/{antenna}/{ddi}'
+                if os.path.isdir(path):
+                    if parallel:
+                        delayed_list.append(dask.delayed(chunk_function)(dask.delayed(parm_dict)))
+                    else:
+                        chunk_function(parm_dict)
+                    count += 1
+                else:
+                    logger.warning(f'[{function_name}]: DDI {ddi} is not present for {antenna}')
+        else:
+            logger.warning(f'[{function_name}]: Antenna {antenna} is not present in {parm_dict["filename"]}')
+
+    if parallel:
+        dask.compute(delayed_list)
+
+    if count == 0:
+        logger.warning(f"[{function_name}]: No data to process")
+    return count
+
