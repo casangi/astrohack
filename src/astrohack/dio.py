e002892a9b4c183110cdad4d9ec403ebdd4ed427
Victor de Souza magalhaes <vdesouza@nrao.edu>
2023-05-23 15:06:21 -0600

    refactoring & standardization: Removed unused imports and export_to_fits from dio user facing file

--- src/astrohack/dio.py
+++ src/astrohack/dio.py
@@ -1,23 +1,13 @@
-import os
-import numbers
 import numpy as np
-from matplotlib import colormaps as cmaps
 
 from casacore import tables
 
-from astrohack._utils._dio import _check_mds_origin, check_if_file_exists
-from astrohack._utils._constants import length_units, trigo_units, plot_types
-from astrohack._utils._parm_utils._check_parms import _check_parms
 from astrohack._utils._logger._astrohack_logger import _get_astrohack_logger
-from astrohack._utils._dask_graph_tools import _generate_antenna_ddi_graph_and_compute
 
 from astrohack._utils._mds import AstrohackImageFile
 from astrohack._utils._mds import AstrohackHologFile
 from astrohack._utils._mds import AstrohackPanelFile
 from astrohack._utils._mds import AstrohackPointFile
-
-from astrohack._utils._panel import _plot_antenna_chunk, _export_to_fits_panel_chunk, _export_screws_chunk
-from astrohack._utils._holog import _export_to_fits_holog_chunk
 
 
 def open_holog(file):
@@ -208,81 +198,3 @@ def fix_pointing_table(ms_name, reference_antenna):
         length = len(message)
         tb.putcol(columnname="MESSAGE", value='pnt_tbl:fixed', startrow=length)
 
-
-def export_to_fits(mds_name, destination, complex_split='cartesian', ant_name=None, ddi=None, parallel=True):
-    """ Export contents of an Astrohack MDS file to several FITS files in the destination folder
-
-    :param mds_name: Input panel_mds file
-    :type mds_name: str
-    :param destination: Name of the destination folder to contain plots
-    :type destination: str
-    :param complex_split: How to split complex data, cartesian (real + imaginary) or polar (amplitude + phase)
-    :type complex_split: str
-    :param ant_name: List of antennae/antenna to be plotted, defaults to "all" when None
-    :type ant_name: list or str, optional, ex. ant_ea25
-    :param ddi: List of ddis/ddi to be plotted, defaults to "all" when None
-    :type ddi: list or str, optional, ex. ddi_0
-    :param parallel: If True will use an existing astrohack client to produce plots in parallel
-    :type parallel: bool
-
-    .. _Description:
-    Export the products from either holog or panel onto FITS files to be read by other software packages
-
-    **Additional Information**
-    The image products of holog are complex images due to the nature of interferometric measurements and Fourier
-    transforms, currently complex128 FITS files are not supported by astropy, hence the need to split complex images
-    onto two real image products, we present the user with two options to carry out this split.
-
-        .. rubric:: Available complex splitting possibilities:
-        - *cartesian*: Split is done to a real part and an imaginary part FITS files
-        - *polar*:     Split is done to an amplitude and a phase FITS files
-
-
-    The FITS produced by this function have been tested are known to work with CARTA and DS9
-    """
-
-    logger = _get_astrohack_logger()
-    parm_dict = {'filename': mds_name,
-                 'ant_name': ant_name,
-                 'ddi': ddi,
-                 'destination': destination,
-                 'complex_split': complex_split,
-                 'parallel': parallel}
-
-    parms_passed = _check_parms(parm_dict, 'filename', [str], default=None)
-    parms_passed = parms_passed and _check_parms(parm_dict, 'complex_split', [str],
-                                                 acceptable_data=['cartesian', 'polar'], default="cartesian")
-    parms_passed = parms_passed and _check_parms(parm_dict, 'ant_name', [list], list_acceptable_data_types=[str],
-                                                 default='all')
-    parms_passed = parms_passed and _check_parms(parm_dict, 'ddi', [list], list_acceptable_data_types=[str],
-                                                 default='all')
-    parms_passed = parms_passed and _check_parms(parm_dict, 'destination', [str], default=None)
-    parms_passed = parms_passed and _check_parms(parm_dict, 'parallel', [bool], default=True)
-
-    if not parms_passed:
-        logger.error("export_screws parameter checking failed.")
-        raise Exception("export_screws parameter checking failed.")
-
-    check_if_file_exists(mds_name)
-    mds_origin = _check_mds_origin(mds_name, ['image', 'panel', 'point', 'holog'])
-
-    if mds_origin in ['combine', 'holog']:
-        chunk_function = _export_to_fits_holog_chunk
-        holog_mds = AstrohackImageFile(mds_name)
-        holog_mds.open()
-        parm_dict['holog_mds'] = holog_mds
-    elif mds_origin == 'panel':
-        chunk_function = _export_to_fits_panel_chunk
-        panel_mds = AstrohackPanelFile(mds_name)
-        panel_mds.open()
-        parm_dict['panel_mds'] = panel_mds
-    else:
-        logger.error(f"Cannot export mds_files created by {mds_origin} to FITS")
-        return
-
-    try:
-        os.mkdir(parm_dict['destination'])
-    except FileExistsError:
-        logger.warning('Destination folder already exists, results may be overwritten')
-
-    _generate_antenna_ddi_graph_and_compute('export_to_fits', chunk_function, parm_dict, parallel)
