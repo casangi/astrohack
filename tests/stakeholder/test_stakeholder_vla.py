f1b08b84976304ff277553815b436c3c836facb9
Joshua Hoskins <jrh4dy@gmail.com>
2023-08-31 13:19:54 -0400

    Add new functionality to stakeholder tests.

--- tests/stakeholder/test_stakeholder_vla.py
+++ tests/stakeholder/test_stakeholder_vla.py
@@ -8,8 +8,6 @@ import astrohack
 
 import numpy as np
 
-from astrohack.gdown_utils import gdown_data
-
 from astrohack.extract_pointing import extract_pointing
 from astrohack.extract_holog import extract_holog
 from astrohack.holog import holog
@@ -29,12 +27,12 @@ def set_data(tmp_path_factory):
   data_dir = tmp_path_factory.mktemp("data")
     
   # Data files
-  astrohack.gdown_utils.download('ea25_cal_small_before_fixed.split.ms', folder=str(data_dir), unpack=True)
-  astrohack.gdown_utils.download('ea25_cal_small_after_fixed.split.ms', folder=str(data_dir), unpack=True)
+  astrohack.astrohack.data.datasets.download('ea25_cal_small_before_fixed.split.ms', folder=str(data_dir), unpack=True)
+  astrohack.astrohack.data.datasets.download('ea25_cal_small_after_fixed.split.ms', folder=str(data_dir), unpack=True)
 
   # Verification json information
-  astrohack.gdown_utils.download(file='extract_holog_verification.json', folder=str(data_dir))
-  astrohack.gdown_utils.download(file='holog_numerical_verification.json', folder=str(data_dir))
+  astrohack.astrohack.data.datasets.download(file='extract_holog_verification.json', folder=str(data_dir))
+  astrohack.astrohack.data.datasets.download(file='holog_numerical_verification.json', folder=str(data_dir))
 
   return data_dir
 
