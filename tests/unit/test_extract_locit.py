83f6a1bbd84a82935ae63a26ca852076f8815b01
Neal <nschweig@nrao.edu>
2023-09-20 11:18:47 -0400

    added comments to test cases and cleaned up some unnessisary variables

--- tests/unit/test_extract_locit.py
+++ tests/unit/test_extract_locit.py
@@ -10,15 +10,12 @@ from astrohack.extract_locit import extract_locit
 class TestExtractHolog():
     cal_table = './data/locit-input-pha.cal'
     locit_name = './data/locit-input-pha.locit.zarr'
-    #position_name = './data/locit-input-pha.position.zarr'
 
     @classmethod
     def setup_class(cls):
         """ setup any state specific to the execution of the given test class
         such as fetching test data """
         astrohack.data.datasets.download(file="locit-input-pha.cal", folder="data")
-        #astrohack.data.datasets.download(file="locit-input-pha.locit.zarr'", folder="data")
-        #astrohack.data.datasets.download(file="locit-input-pha.position.zarr", folder="data")
         
     @classmethod
     def teardown_class(cls):
@@ -31,6 +28,7 @@ class TestExtractHolog():
 
     def test_extract_locit_simple(self):
     
+        # Create locit_mds and check the dictionary structure
         locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name)
         
         expected_keys = ['obs_info', 'ant_info', 'ant_ea01', 'ant_ea02', 'ant_ea04', 'ant_ea05', 'ant_ea06', 'ant_ea07', 'ant_ea08', 'ant_ea09', 'ant_ea10', 'ant_ea11', 'ant_ea12', 'ant_ea13', 'ant_ea15', 'ant_ea16', 'ant_ea17', 'ant_ea18', 'ant_ea19', 'ant_ea20', 'ant_ea21', 'ant_ea22', 'ant_ea23', 'ant_ea24', 'ant_ea25', 'ant_ea26', 'ant_ea27', 'ant_ea28']
@@ -42,6 +40,7 @@ class TestExtractHolog():
     
         locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, ant_id='ea17')
         
+        # There should only be 1 antenna in the dict named ea17
         assert len(locit_mds.keys()) == 3
         assert 'ant_ea17' in locit_mds.keys()
     
@@ -49,6 +48,7 @@ class TestExtractHolog():
     
         locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, ddi=0)
         
+        # Usually each antenna has a ddi_0 and ddi_1 with all selected. This should just give ddi_0
         assert len(locit_mds['ant_ea01'].keys()) == 1
         assert 'ddi_0' in locit_mds['ant_ea01'].keys()
         
@@ -56,6 +56,7 @@ class TestExtractHolog():
     
         locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name)
         
+        # an exception shouldn't be raised with overwrite set to True
         try:
             locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, overwrite=True)
         except:
@@ -65,6 +66,7 @@ class TestExtractHolog():
     
         locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, overwrite=False)
         
+        # with overwrite set to False an exception should be raised
         try:
             locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, overwrite=False)
         except:
