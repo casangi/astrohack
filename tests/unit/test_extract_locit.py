6e3392d9beff43d9f633f72a2059941aad47c583
Joshua Hoskins <jrh4dy@gmail.com>
2023-10-04 10:53:16 -0400

    fix current extract_locit tests and add docstings more tests.

--- tests/unit/test_extract_locit.py
+++ tests/unit/test_extract_locit.py
@@ -1,8 +1,10 @@
+import os
 import pytest
 import shutil
 import astrohack
 
 from astrohack.extract_locit import extract_locit
+
 
 class TestExtractLocit():
     cal_table = './data/locit-input-pha.cal'
@@ -13,58 +15,98 @@ class TestExtractLocit():
         """ setup any state specific to the execution of the given test class
         such as fetching test data """
         astrohack.data.datasets.download(file="locit-input-pha.cal", folder="data")
-        
+
     @classmethod
     def teardown_class(cls):
         """ teardown any state that was previously setup with a call to setup_class
         such as deleting test data """
         shutil.rmtree("data")
-        
+
     def teardown_method(self):
         shutil.rmtree(self.locit_name)
 
-    def test_extract_locit_simple(self):
-    
+    def test_extract_locit_creation(self):
+        """
+           Create locit file with a cal-table and check it is created correctly.
+        """
+
         # Create locit_mds and check the dictionary structure
         locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name)
-        
-        expected_keys = ['obs_info', 'ant_info', 'ant_ea01', 'ant_ea02', 'ant_ea04', 'ant_ea05', 'ant_ea06', 'ant_ea07', 'ant_ea08', 'ant_ea09', 'ant_ea10', 'ant_ea11', 'ant_ea12', 'ant_ea13', 'ant_ea15', 'ant_ea16', 'ant_ea17', 'ant_ea18', 'ant_ea19', 'ant_ea20', 'ant_ea21', 'ant_ea22', 'ant_ea23', 'ant_ea24', 'ant_ea25', 'ant_ea26', 'ant_ea27', 'ant_ea28']
-        
+
+        expected_keys = ['obs_info', 'ant_info', 'ant_ea01', 'ant_ea02', 'ant_ea04', 'ant_ea05', 'ant_ea06', 'ant_ea07',
+                         'ant_ea08', 'ant_ea09', 'ant_ea10', 'ant_ea11', 'ant_ea12', 'ant_ea13', 'ant_ea15', 'ant_ea16',
+                         'ant_ea17', 'ant_ea18', 'ant_ea19', 'ant_ea20', 'ant_ea21', 'ant_ea22', 'ant_ea23', 'ant_ea24',
+                         'ant_ea25', 'ant_ea26', 'ant_ea27', 'ant_ea28']
+
         for key in locit_mds.keys():
             assert key in expected_keys
-        
+
+        assert os.path.exists(self.locit_name)
+
     def test_extract_locit_antenna_select(self):
-    
-        locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, ant_id='ea17')
-        
+        """
+           Check that only specified antenna is processed.
+        """
+
+        locit_mds = extract_locit(
+            self.cal_table,
+            locit_name=self.locit_name,
+            ant_id='ea17'
+        )
+
         # There should only be 1 antenna in the dict named ea17
         assert len(locit_mds.keys()) == 3
-        assert 'ant_ea17' in locit_mds.keys()
-    
+
+        # Check that only the specific antenna is in the keys.
+        assert list(locit_mds.keys()) == ['ant_ea17']
+
     def test_extract_locit_ddi(self):
-    
+        """
+           Check that only specified ddi is processed.
+        """
+
         locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, ddi=0)
-        
-        # Usually each antenna has a ddi_0 and ddi_1 with all selected. This should just give ddi_0
+
+        # Check that only the specific ddi is in the keys.
         assert len(locit_mds['ant_ea01'].keys()) == 1
-        assert 'ddi_0' in locit_mds['ant_ea01'].keys()
-        
+        assert list(locit_mds['ant_ea01'].keys()) == ["ddi_0"]
+
     def test_extract_locit_overwrite(self):
-    
-        locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name)
-        
-        # an exception shouldn't be raised with overwrite set to True
-        try:
-            locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, overwrite=True)
-        except:
-            assert False, "Failed to overwrite"
-            
+        """
+           Specify the output file should be overwritten; check that it WAS.
+        """
+
+        # To check this properly we need to not only know an exception was not thrown but that the file is ACTUALLY
+        # overwritten. We do this by checking the modification time.
+        initial_time = os.path.getctime(self.locit_name)
+
+        extract_locit(
+            self.cal_table,
+            locit_name=self.locit_name,
+            overwrite=True
+        )
+
+        modified_time = os.path.getctime(self.locit_name)
+
+        assert initial_time != modified_time
+
     def test_extract_locit_no_overwrite(self):
-    
-        locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, overwrite=False)
-        
-        # with overwrite set to False an exception should be raised
+        """
+            Specify the output file should be NOT be overwritten; check that it WAS NOT.
+        """
+        initial_time = os.path.getctime(self.locit_name)
+
         try:
-            locit_mds = extract_locit(self.cal_table, locit_name=self.locit_name, overwrite=False)
-        except:
+            extract_locit(
+                self.cal_table,
+                locit_name=self.locit_name,
+                overwrite=False
+            )
+
+        except FileExistsError:
             pass
+
+        finally:
+            modified_time = os.path.getctime(self.locit_name)
+
+            assert initial_time == modified_time
