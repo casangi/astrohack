- Fix reappearing bug in default file name function. - Update vla tests now that changes to cell_size, grid_size and Victor's averaging are done. - Remove ALMA testing for now since it isn't officially supported.

tests/stakeholder/test_stakeholder_vla.py
@@ -33,13 +33,14 @@ def relative_difference(result, expected):
 
 def verify_panel_shifts(
         data_dir="",
-        panel_list=['3-4', '5-27', '5-37', '5-38'],
+        panel_list=None,
         expected_shift=np.array([-100, 75, 0, 150]),
-        ref_mean_shift=np.array([-112.23760235, 73.09423151, -1.52957784, 138.96735818]),
+        ref_mean_shift=np.array([-77.8000519, 49.8347927, -0.0476941708, 100.268957]),
         antenna='ant_ea25',
         ddi='ddi_0'
 ):
-
+    if panel_list is None:
+        panel_list = ['3-4', '5-27', '5-37', '5-38']
 
     M_TO_MILS = 39370.1
 
@@ -52,12 +53,15 @@ def verify_panel_shifts(
     difference = after_shift - before_shift
 
     mean_shift = np.mean(difference, axis=1)
+    print(mean_shift)
 
     delta_mean_shift = np.abs(mean_shift - expected_shift)
     delta_ref_shift = np.abs(ref_mean_shift - expected_shift)
 
-    delta_shift = delta_mean_shift - delta_ref_shift  # New corrections - old corrections --> delta if delta < 0 ==> we improved.
+    # New corrections - old corrections --> delta if delta < 0 ==> we improved.
+    delta_shift = delta_mean_shift - delta_ref_shift
     relative_shift = relative_difference(delta_mean_shift, delta_ref_shift)
+    print(relative_shift)
 
     return np.all(relative_shift < 1e-6)
 
@@ -123,7 +127,7 @@ def verify_holog_diagnostics(json_data, truth_json, tolerance=1e-7):
     cell_size = np.abs(float(cell_size))
 
     return (relative_difference(json_data['cell_size'], cell_size) < tolerance) and (
-                relative_difference(np.sqrt(int(json_data['n_pix'])), grid_size) < tolerance)
+            relative_difference(int(np.sqrt(json_data['n_pix'])), grid_size) < tolerance)
 
 
 def test_holography_pipeline(set_data):
@@ -162,8 +166,6 @@ def test_holography_pipeline(set_data):
         overwrite=True
     )
 
-    assert verify_holog_obs_dictionary(holog_obs_dict["vla"]["before"])
-
     extract_holog(
         ms_name=after_ms,
         point_name=after_point,
@@ -173,7 +175,7 @@ def test_holography_pipeline(set_data):
         overwrite=True
     )
 
-    with open(str(set_data / "vla.before.split.holog.zarr/.holog_attr")) as attr_file:
+    with open(str(set_data / "vla.after.split.holog.zarr/.holog_attr")) as attr_file:
         holog_attr = json.load(attr_file)
 
     assert verify_holog_diagnostics(
@@ -211,7 +213,7 @@ def test_holography_pipeline(set_data):
         antenna='ant_ea25',
         ddi='ddi_0',
         reference_center_pixels=reference_dict["vla"]["pixels"]["before"],
-        tolerance=1e-6
+        tolerance=1.5e-6
     )
 
     holog(
@@ -232,7 +234,7 @@ def test_holography_pipeline(set_data):
         antenna='ant_ea25',
         ddi='ddi_0',
         reference_center_pixels=reference_dict["vla"]["pixels"]["after"],
-        tolerance=1e-6
+        tolerance=1.5e-6
     )
 
     before_image = str(set_data / "vla.before.split.image.zarr")
@@ -241,9 +243,6 @@ def test_holography_pipeline(set_data):
     before_panel = panel(
         image_name=before_image,
         panel_model='rigid',
-        clip_type='relative',
-        clip_level=0.2,
-        panel_margins=0.2,
         parallel=False,
         overwrite=True
     )
@@ -251,9 +250,6 @@ def test_holography_pipeline(set_data):
     after_panel = panel(
         image_name=after_image,
         panel_model='rigid',
-        clip_type='relative',
-        clip_level=0.2,
-        panel_margins=0.2,
         parallel=False,
         overwrite=True
     )