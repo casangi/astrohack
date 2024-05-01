fc0396896aea8d4a0ac17fc113b3d414a3583234
Joshua Hoskins <jrh4dy@gmail.com>
2024-05-01 13:33:18 -0400

    Add encrypted uploader for verification files to dropbox.

--- src/astrohack/utils/veritas.py
+++ src/astrohack/utils/veritas.py
@@ -1,7 +1,6 @@
-import os
 import json
-import pathlib
 import dropbox
+import pathlib
 
 import pandas as pd
 import numpy as np
@@ -73,109 +72,197 @@ def generate_verification_json(path, antenna, ddi, write=False):
     return numerical_dict
 
 
-def connect():
-    """Create a connection to Dropbox."""
-    #ACCESS_TOKEN = os.getenv("SECRET_TOKEN")
-    ACCESS_TOKEN = ""
+class Veritas:
 
-    try:
-        dbx = dropbox.Dropbox(ACCESS_TOKEN)
-    except AuthError as e:
-        logger.error('Error connecting to Dropbox with access token: ' + str(e))
-    return dbx
+    def __init__(self):
+        self.dbx = None
+        self.key = None
+        self.secret = None
+        self.certificate_path = None
+
+    def __del__(self):
+        self.dbx.close()
+
+    def connect(self, certificate_path=None):
+        """Create a connection to Dropbox."""
+
+        try:
+            self.decrypt_data(certificate_path)
+            self.connect_o2_auth()
+            logger.info("Connected to Dropbox")
+
+        except AuthError as e:
+            logger.error('Error connecting to Dropbox with access token: ' + str(e))
+
+    def list_files(self, path):
+        """Return a Pandas dataframe of files in a given Dropbox folder path in the Apps directory.
+        """
+
+        if self.dbx is None:
+            logger.info("No connection to dropbox. Run connect() command.")
+            return None
+
+        try:
+            files = self.dbx.files_list_folder(path).entries
+            files_list = []
+            for file in files:
+                if isinstance(file, dropbox.files.FileMetadata):
+                    metadata = {
+                        'name': file.name,
+                        'path_display': file.path_display,
+                        'client_modified': file.client_modified,
+                        'server_modified': file.server_modified
+                    }
+                    files_list.append(metadata)
+
+            df = pd.DataFrame.from_records(files_list)
+            return df.sort_values(by='server_modified', ascending=False)
+
+        except Exception as e:
+            logger.error('Error getting list of files from Dropbox: ' + str(e))
+
+    def download_file(self, dropbox_file_path, local_file_path):
+        """Download a file from Dropbox to the local machine."""
+
+        if self.dbx is None:
+            logger.info("No connection to dropbox. Run connect() command.")
+            return None
+
+        try:
+            with open(local_file_path, 'wb') as f:
+                metadata, result = self.dbx.files_download(path=dropbox_file_path)
+                f.write(result.content)
+
+        except Exception as e:
+            logger.error('Error downloading file from Dropbox: ' + str(e))
+
+    def upload_file(self, local_path, local_file, dropbox_file_path):
+        """Upload a file from the local machine to a path in the Dropbox app directory.
+
+        Args:
+            local_path (str): The path to the local file.
+            local_file (str): The name of the local file.
+            dropbox_file_path (str): The path to the file in the Dropbox app directory.
+
+        Example:
+            dropbox_upload_file('.', 'test.csv', '/stuff/test.csv')
+
+        Returns:
+            meta: The Dropbox file metadata.
+        """
+
+        if self.dbx is None:
+            logger.info("No connection to dropbox. Run connect() command.")
+            return None
+
+        try:
+
+            local_file_path = pathlib.Path(local_path) / local_file
+
+            with local_file_path.open("rb") as f:
+                meta = self.dbx.files_upload(f.read(), dropbox_file_path, mode=dropbox.files.WriteMode("overwrite"))
+
+                return meta
+        except Exception as e:
+            logger.error('Error uploading file to Dropbox: ' + str(e))
+
+    def connect_o2_auth(self):
+        from dropbox import DropboxOAuth2FlowNoRedirect
+
+        '''
+        This example walks through a basic oauth flow using the existing long-lived token type
+        Populate your app key and app secret in order to run this locally
+        '''
+
+        auth_flow = DropboxOAuth2FlowNoRedirect(self.key, self.secret)
+
+        authorize_url = auth_flow.start()
+        print("1. Go to: " + authorize_url)
+        print("2. Click \"Allow\" (you might have to log in first).")
+        print("3. Copy the authorization code.")
+        auth_code = input("Enter the authorization code here: ").strip()
+
+        try:
+            oauth_result = auth_flow.finish(auth_code)
+            self.dbx = dropbox.Dropbox(oauth2_access_token=oauth_result.access_token)
+
+        except Exception as e:
+            logger.error('Error: %s' % (e,))
+            exit(1)
+
+    def decrypt_data(self, certificate_path=None):
+        from Crypto.PublicKey import RSA
+        from Crypto.Cipher import AES, PKCS1_OAEP
+
+        if certificate_path is not None:
+            self.certificate_path = certificate_path
+
+        try:
+            private_key = RSA.import_key(open(f"{self.certificate_path}/private.pem").read())
+
+            with open(f"{self.certificate_path}/encrypted_data.bin", "rb") as f:
+                enc_session_key = f.read(private_key.size_in_bytes())
+                nonce = f.read(16)
+                tag = f.read(16)
+                ciphertext = f.read()
+
+            # Decrypt the session key with the private RSA key
+            cipher_rsa = PKCS1_OAEP.new(private_key)
+            session_key = cipher_rsa.decrypt(enc_session_key)
+
+            # Decrypt the data with the AES session key
+            cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+            data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+
+            token = data.decode("utf-8")
+            self.key, self.secret = token.split(":")
+
+        except Exception as error:
+            logger.error('Error: %s' % (error,))
+            raise AuthError
 
 
-def list_files(path):
-    """Return a Pandas dataframe of files in a given Dropbox folder path in the Apps directory.
-    """
+def generate_rsa_key():
+    from Crypto.PublicKey import RSA
 
-    dbx = connect()
+    if not pathlib.Path(".keys").exists():
+        pathlib.Path(".keys").mkdir(parents=True, exist_ok=True)
 
-    try:
-        files = dbx.files_list_folder(path).entries
-        files_list = []
-        for file in files:
-            if isinstance(file, dropbox.files.FileMetadata):
-                metadata = {
-                    'name': file.name,
-                    'path_display': file.path_display,
-                    'client_modified': file.client_modified,
-                    'server_modified': file.server_modified
-                }
-                files_list.append(metadata)
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    with open(".keys/private.pem", "wb") as f:
+        f.write(private_key)
 
-        df = pd.DataFrame.from_records(files_list)
-        return df.sort_values(by='server_modified', ascending=False)
-
-    except Exception as e:
-        logger.error('Error getting list of files from Dropbox: ' + str(e))
+    public_key = key.publickey().export_key()
+    with open(".keys/receiver.pem", "wb") as f:
+        f.write(public_key)
 
 
-def download_file(dropbox_file_path, local_file_path):
-    """Download a file from Dropbox to the local machine."""
+def encrypt_data(key: str, secret: str):
+    from Crypto.PublicKey import RSA
+    from Crypto.Random import get_random_bytes
+    from Crypto.Cipher import AES, PKCS1_OAEP
 
-    try:
-        dbx = connect()
+    generate_rsa_key()
 
-        with open(local_file_path, 'wb') as f:
-            metadata, result = dbx.files_download(path=dropbox_file_path)
-            f.write(result.content)
-    except Exception as e:
-        logger.error('Error downloading file from Dropbox: ' + str(e))
+    data = ":".join((key, secret)).encode("utf-8")
 
+    recipient_key = RSA.import_key(open(".keys/receiver.pem").read())
+    session_key = get_random_bytes(16)
 
-def upload_file(local_path, local_file, dropbox_file_path):
-    """Upload a file from the local machine to a path in the Dropbox app directory.
+    # Encrypt the session key with the public RSA key
 
-    Args:
-        local_path (str): The path to the local file.
-        local_file (str): The name of the local file.
-        dropbox_file_path (str): The path to the file in the Dropbox app directory.
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    enc_session_key = cipher_rsa.encrypt(session_key)
 
-    Example:
-        dropbox_upload_file('.', 'test.csv', '/stuff/test.csv')
+    # Encrypt the data with the AES session key
 
-    Returns:
-        meta: The Dropbox file metadata.
-    """
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
 
-    try:
-        dbx = connect()
+    with open(".keys/encrypted_data.bin", "wb") as f:
+        f.write(enc_session_key)
+        f.write(cipher_aes.nonce)
+        f.write(tag)
+        f.write(ciphertext)
 
-        local_file_path = pathlib.Path(local_path) / local_file
-
-        with local_file_path.open("rb") as f:
-            meta = dbx.files_upload(f.read(), dropbox_file_path, mode=dropbox.files.WriteMode("overwrite"))
-
-            return meta
-    except Exception as e:
-        logger.error('Error uploading file to Dropbox: ' + str(e))
-
-
-def connect_o2_auth():
-    from dropbox import DropboxOAuth2FlowNoRedirect
-
-    '''
-    This example walks through a basic oauth flow using the existing long-lived token type
-    Populate your app key and app secret in order to run this locally
-    '''
-    APP_KEY = ""
-    APP_SECRET = ""
-
-    auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
-
-    authorize_url = auth_flow.start()
-    print("1. Go to: " + authorize_url)
-    print("2. Click \"Allow\" (you might have to log in first).")
-    print("3. Copy the authorization code.")
-    auth_code = input("Enter the authorization code here: ").strip()
-
-    try:
-        oauth_result = auth_flow.finish(auth_code)
-    except Exception as e:
-        logger.error('Error: %s' % (e,))
-        exit(1)
-
-    with dropbox.Dropbox(oauth2_access_token=oauth_result.access_token) as dbx:
-        dbx.users_get_current_account()
-        logger.info("Successfully set up client!")
