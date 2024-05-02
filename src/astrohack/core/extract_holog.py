Fixed numba errors in time averaging due to segmentation faults caused by wild indexes

src/astrohack/core/extract_holog.py
@@ -3,7 +3,6 @@ import json
 import numpy as np
 import xarray as xr
 import astropy
-import astrohack
 import graphviper.utils.logger as logger
 
 from numba import njit
@@ -174,7 +173,8 @@ def _get_time_intervals(time_vis_row, scan_list, time_interval):
             if time_range[0] <= time_sample <= time_range[1]:
                 filtered_time_samples.append(time_sample)
                 break
-    return np.array(filtered_time_samples)
+    time_samples = np.array(filtered_time_samples)
+    return time_samples
 
 
 @njit(cache=False, nogil=True)
@@ -224,10 +224,12 @@ def _extract_holog_chunk_jit(
 
     for antenna_id in map_ant_tuple:
         vis_map_dict[antenna_id] = np.zeros(
-            (n_time, n_chan, n_pol), dtype=types.complex64
+            (n_time, n_chan, n_pol),
+            dtype=types.complex128,
         )
         sum_weight_map_dict[antenna_id] = np.zeros(
-            (n_time, n_chan, n_pol), dtype=types.float64
+            (n_time, n_chan, n_pol),
+            dtype=types.float64,
         )
         used_samples_dict[antenna_id] = np.full(n_time, False, dtype=bool)
 
@@ -235,13 +237,14 @@ def _extract_holog_chunk_jit(
     for row in range(n_row):
         if flag_row is False:
             continue
+
         # Find index of time_vis_row[row] in time_samples, assumes time_vis_row is ordered in time
 
         if time_vis_row[row] < time_samples[time_index] - half_int:
             continue
-        while time_vis_row[row] > time_samples[time_index] + half_int:
-            time_index += 1
-        if time_index == n_time:
+        else:
+            time_index = _get_time_index(time_vis_row[row], time_index, time_samples, half_int)
+        if time_index < 0:
             break
 
         ant1_id = ant1[row]
@@ -613,11 +616,10 @@ def _time_avg_pointing_jit(time_vis, pnt_time, dire, dir_cos, enc, pnt_off, tgt)
     for i_row in range(n_row):
         if pnt_time[i_row] < time_vis[i_time] - half_int:
             continue
-        while pnt_time[i_row] > time_vis[i_time] + half_int:
-            i_time += 1
-        if i_time == n_samples:
+        else:
+            i_time = _get_time_index(pnt_time[i_row], i_time, time_vis, half_int)
+        if i_time < 0:
             break
-
         avg_dir[i_time] += dire[i_row]
         avg_dir_cos[i_time] += dir_cos[i_row]
         avg_enc[i_time] += enc[i_row]
@@ -716,3 +718,14 @@ def create_holog_meta_data(holog_file, holog_dict, input_params):
     meta_data.update(input_params)
 
     return meta_data
+
+
+@njit(cache=False, nogil=True)
+def _get_time_index(data_time, i_time, time_axis, half_int):
+    if i_time == time_axis.shape[0]:
+        return -1
+    while data_time > time_axis[i_time] + half_int:
+        i_time += 1
+        if i_time == time_axis.shape[0]:
+            return -1
+    return i_time