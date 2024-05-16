237a102eb9d4093542959ed6cfb81f888efc9727
Victor de Souza magalhaes <vdesouza@nrao.edu>
2024-05-16 15:20:56 -0600

    Removed clutter from jitclass experiment

--- src/astrohack/utils/covolution_gridding.py
+++ src/astrohack/utils/covolution_gridding.py
@@ -1,124 +1,19 @@
 import time
-
 import numpy as np
 from graphviper.utils import logger as logger
 from numba import njit
-from numba.experimental import jitclass
 from numba.core import types
 from matplotlib import pyplot as plt
 
 from astrohack.utils import calc_coords
 
 
-@njit(cache=False, nogil=True)
-def find_nearest(value, array):
-    diff = np.abs(array-value)
-    idx = diff.argmin()
-    return idx
-
-
-class_interface = [('bias', types.float64),
-                   ('u_axis', types.float64[:]),
-                   ('kernel', types.float64[:]),
-                   ('user_support', types.float64),
-                   ('user_width', types.float64),
-                   ('pix_support', types.float64),
-                   ('cell_size', types.float64)]
-
-
-@jitclass(class_interface)
-class ExponentialConvolutionKernel:
-    def __init__(self, beam_size, exponent, cell_size):
-        smoothing = beam_size
-        support = 4*smoothing
-        width = smoothing/(2*np.sqrt(np.log(2.0)))
-
-        pix_support = support/np.abs(cell_size)
-        pix_width = width/np.abs(cell_size)
-        if pix_support < 1.0:
-            used_support = 2*(pix_support + 0.995) + 1
-        else:
-            used_support = 2*pix_support + 1
-        kernel_size = used_support * 100 + 1
-        self.bias = 50.0 * used_support + 1.0
-        self.u_axis = (np.arange(kernel_size)-self.bias)*0.01
-        self.kernel = np.exp(-(self.u_axis/pix_width)**exponent)
-        self.user_support = support
-        self.user_width = width
-        self.pix_support = pix_support
-        self.cell_size = cell_size
-
-    def factor(self, delta):
-        pix_delta = delta/np.abs(self.cell_size)
-        ikern = round(100.0*pix_delta+self.bias)
-        return self.kernel[ikern]
-
-    def compute_range(self, coor, axis):
-        idx = find_nearest(coor, axis)
-        i_min = idx - self.pix_support
-        i_max = idx + self.pix_support
-
-        if i_min < 0:
-            i_min = 0
-        if i_max >= axis.shape[0]:
-            i_max = axis.shape[0] - 1
-        return i_min, i_max
-
-    def plot(self):
-        plt.plot(self.u_axis, self.kernel)
-        plt.show()
-        plt.clf()
-
-
-
-
-def exponential_convolution_kernel(support, width, exponent):
-    # Number of rows
-    used_support = int(max(support + 0.995, 1.0))
-    print(used_support, width)
-    used_support = used_support * 2 + 1
-    kernel_size = used_support * 100 + 1
-
-
-    bias = 50.0 * used_support + 1.0
-
-    u_axis = np.abs(np.arange(kernel_size)-bias*0.01)
-    u_axis = (np.arange(kernel_size)-bias)*0.01
-    kernel = np.exp(-(u_axis/width)**exponent)
-    #kernel = np.where(u_axis > support, 0, np.exp(-(u_axis/width)**exponent))
-
-    ker_dict = {'kernel': kernel,
-                'bias': bias,
-                'support': support}
-
-    # xaxis = np.arange(kernel_size)
-    # plt.plot(u_axis, kernel)
-    # plt.show()
-
-    return ker_dict
-
-
 def convolution_gridding(grid_type, visibilities, weights, lmaxis, diameter, freq, sky_cell_size, grid_size):
     # This beam size is anchored at NOEMA beam measurements we might need a more general formula
     beam_size = 41*(115e9/freq[0])*np.sqrt(2.)*(15./diameter)*np.pi/180/3600
-    #smoothing = beam_size/2.0
-    # l_support = support/np.abs(sky_cell_size[0])
-    # m_support = support/np.abs(sky_cell_size[1])
-    # l_width = smoothing/(2*np.sqrt(np.log(2.0)))/np.abs(sky_cell_size[0])
-    # m_width = smoothing/(2*np.sqrt(np.log(2.0)))/np.abs(sky_cell_size[1])
-    # print(support, l_width)
-    exponent = 2
 
     if grid_type == 'exponential':
         pass
-        # print('l', l_support, l_width, exponent)
-        # l_kernel = exponential_convolution_kernel(l_support, l_width, exponent)
-        # print('m', m_support, m_width, exponent)
-        # m_kernel = exponential_convolution_kernel(m_support, m_width, exponent)
-        # l_kernel = ExponentialConvolutionKernel(support, width, exponent, sky_cell_size[0])
-        # m_kernel = ExponentialConvolutionKernel(support, width, exponent, sky_cell_size[1])
-        # #l_kernel.plot()
-
     else:
         msg = f'Unknown grid type {grid_type}.'
         logger.error(msg)
@@ -133,69 +28,56 @@ def convolution_gridding(grid_type, visibilities, weights, lmaxis, diameter, fre
     logger.info('Creating convolved beam...')
     start = time.time()
     laxis, maxis = calc_coords(grid_size, sky_cell_size)
-    beam, wei = convolution_gridding_jit(visibilities, lmaxis, weights, sky_cell_size, laxis, maxis, beam_size)
+    beam, wei = _convolution_gridding_jit(visibilities, lmaxis, weights, sky_cell_size, laxis, maxis, beam_size)
     duration = time.time()-start
     logger.info(f'Convolution took {duration:.3} seconds')
     return beam
 
 
 @njit(cache=False, nogil=True)
-def convolution_gridding_jit(visibilities, lmaxis, weights, sky_cell_size, laxis, maxis, beam_size):
+def _convolution_gridding_jit(visibilities, lmaxis, weights, sky_cell_size, laxis, maxis, beam_size):
     ntime, nchan, npol = visibilities.shape
 
     exponent = 2
-    l_kernel = create_exponential_kernel(beam_size, exponent, sky_cell_size[0])
-    m_kernel = create_exponential_kernel(beam_size, exponent, sky_cell_size[1])
+    l_kernel = _create_exponential_kernel(beam_size, exponent, sky_cell_size[0])
+    m_kernel = _create_exponential_kernel(beam_size, exponent, sky_cell_size[1])
 
     grid_shape = (nchan, npol, laxis.shape[0], maxis.shape[0])
     beam_grid = np.zeros(grid_shape, dtype=types.complex128)
     weig_grid = np.zeros(grid_shape)
 
     for i_time in range(ntime):
-        # if i_time % 100 == 0:
-        #     print(f'{100*i_time/ntime:.1f}% done')
 
         lval, mval = lmaxis[i_time]
-        # #print('l')
-        # i_lmin, i_lmax = compute_i_min_max(lval, laxis, l_kernel['support'], sky_cell_size[0], grid_size[0])
-        # #print('m')
-        # i_mmin, i_mmax = compute_i_min_max(mval, maxis, m_kernel['support'], sky_cell_size[1], grid_size[1])
-        i_lmin, i_lmax = compute_kernel_range(l_kernel, lval, laxis)
-        i_mmin, i_mmax = compute_kernel_range(m_kernel, mval, maxis)
+        i_lmin, i_lmax = _compute_kernel_range(l_kernel, lval, laxis)
+        i_mmin, i_mmax = _compute_kernel_range(m_kernel, mval, maxis)
 
         for i_chan in range(nchan):
             for i_pol in range(npol):
                 for il in range(i_lmin, i_lmax):
-                    # dl = (laxis[il]-lval)/sky_cell_size[0]
-                    dl = laxis[il] - lval
-                    l_fac = convolution_factor(l_kernel, dl)
+                    l_fac = _convolution_factor(l_kernel, laxis[il] - lval)
                     for im in range(i_mmin, i_mmax):
-                        # dm = (maxis[im]-mval)/np.abs(sky_cell_size[1])
-                        dm = maxis[im]-mval
-
-                        # print('l', laxis[il], lval, sky_cell_size[0], dl)
-                        # print('m', maxis[im], mval, sky_cell_size[1], dm)
-                        # conv_fact = (conv_factor(dl, l_kernel) * conv_factor(dm, m_kernel) *
-                        #              weights[i_time, i_chan, i_pol])
-
-                        m_fac = convolution_factor(m_kernel, dm)
-                        # print(dl, l_fac)
-                        # print(dm, m_fac)
+                        m_fac = _convolution_factor(m_kernel, maxis[im] - mval)
 
                         conv_fact = l_fac * m_fac * weights[i_time, i_chan, i_pol]
-
                         beam_grid[i_chan, i_pol, il, im] += conv_fact*visibilities[i_time, i_chan, i_pol]
                         weig_grid[i_chan, i_pol, il, im] += conv_fact
 
-    # plt.imshow(np.abs(beam_grid[0, 0, ...]))
-    # plt.show()
-
     beam_grid /= weig_grid
+    beam_grid = np.nan_to_num(beam_grid)
+
     return beam_grid, weig_grid
 
 
 @njit(cache=False, nogil=True)
-def create_exponential_kernel(beam_size, exponent, cell_size):
+def _find_nearest(value, array):
+    diff = np.abs(array-value)
+    idx = diff.argmin()
+    return idx
+
+
+@njit(cache=False, nogil=True)
+def _create_exponential_kernel(beam_size, exponent, cell_size):
     smoothing = beam_size
     support = 4*smoothing
     width = smoothing/(2*np.sqrt(np.log(2.0)))
@@ -222,8 +104,8 @@ def create_exponential_kernel(beam_size, exponent, cell_size):
 
 
 @njit(cache=False, nogil=True)
-def compute_kernel_range(kernel, coor, axis):
-    idx = find_nearest(coor, axis)
+def _compute_kernel_range(kernel, coor, axis):
+    idx = _find_nearest(coor, axis)
     i_min = idx - kernel['pix_support']
     i_max = idx + kernel['pix_support']
 
@@ -235,37 +117,9 @@ def compute_kernel_range(kernel, coor, axis):
 
 
 @njit(cache=False, nogil=True)
-def convolution_factor(kernel, delta):
+def _convolution_factor(kernel, delta):
     pix_delta = delta/np.abs(kernel['cell_size'])
     ikern = round(100.0*pix_delta+kernel['bias'])
     return kernel['kernel'][ikern]
 
 
-@njit(cache=False, nogil=True)
-def compute_i_min_max(coor, axis, support, cell_size, grid_size):
-    idx = find_nearest(coor, axis)
-    pix_support = np.ceil(np.abs(support/cell_size))[0]
-    i_min = idx - support
-    i_max = idx + support
-
-    if i_min < 0:
-        i_min = 0
-    if i_max >= grid_size:
-        i_max = grid_size - 1
-
-    # print(idx, type(idx))
-    # print(support)
-    # print(pix_support, type(pix_support))
-    # print(i_min, type(i_min))
-    # print(i_max, type(i_max))
-
-    return int(i_min), int(i_max)
-
-
-@njit(cache=False, nogil=True)
-def conv_factor(delta, kernel):
-    # print(delta)
-    ikern = round(100.0*delta+kernel['bias'])
-    value = kernel['kernel'][ikern]
-    # print(value)
-    return value
