8a3b77d26984a875a967ac03b4a7fac4588ba7ab
Victor de Souza magalhaes <vdesouza@nrao.edu>
2024-09-10 15:23:00 -0600

    notebook cleanup also cleans holog_obs_dict.json

--- docs/cleanup-notebooks.sh
+++ docs/cleanup-notebooks.sh
@@ -4,7 +4,7 @@ rm -r holog_exports
 rm -r panel_exports
 rm -r .baseline_distance_matrix.csv
 rm -r .holog_diagnostic.json
-rm -r .holog_obs_dict.json
+rm -r holog_obs_dict.json
 rm -r locit_mds_plots
 rm -r position_mds_exports
 rm -r exports
