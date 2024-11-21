0fbec400c8babce30cf3a663b5ad129193e1e9c6
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2024-11-21 11:19:03 -0700

    Improved installation script

--- etc/installation/astrohack-install.sh
+++ etc/installation/astrohack-install.sh
@@ -4,38 +4,35 @@ echo 'Astrohack environment creation and installation script'
 echo 
 
 location=${HOME}'/.local/python/venvs'
-echo -e 'Default installation in '${location}
-echo 'Press <enter> to continue or type desired location for installation.'
-read user_location
+echo -e 'Default installation in '"${location}"
+read -r -p 'Press <enter> to continue or type desired location for installation.' user_location
 if [ -z "${user_location}" ]; then
-    echo 'Using '${location}' for installation'
+    echo 'Using '"${location}"' for installation'
 else
-    echo -e 'Using '${user_location}' for installation'
+    echo -e 'Using '"${user_location}"' for installation'
     location=${user_location}
 fi
 echo 
 
 
 pyexec="$(which python3.11)"
-echo 'Default python executable is '${pyexec}
-echo 'Press <enter> to continue or type desired python executable'
-read user_pyexec
+echo 'Default python executable is '"${pyexec}"
+read -r -p 'Press <enter> to continue or type desired python executable' user_pyexec
 if [ -z "${user_pyexec}" ]; then
-    echo 'Using '${pyexec}' as the python executable'
+    echo 'Using '"${pyexec}"' as the python executable'
 else
-    echo 'Using '${user_pyexec}' as the python executable'
+    echo 'Using '"${user_pyexec}"' as the python executable'
     pyexec=${user_pyexec}
 fi
 echo 
 
 envname="astrohack-py3.11"
 echo 'Default environment name is '${envname}
-echo 'Press <enter> to continue or type desired environment name'
-read user_envname
+read -r -p 'Press <enter> to continue or type desired environment name' user_envname
 if [ -z "${user_envname}" ]; then
     echo 'Using '${envname}' as environment name'
 else
-    echo 'Using '${user_envname}' as environment name'
+    echo 'Using '"${user_envname}"' as environment name'
     envname=${user_envname}
 fi
 echo 
@@ -56,27 +53,28 @@ eval "pip install astrohack 1> /dev/null"
 echo
 
 echo 'Add convenience functions <activate_astrohack> and <get_locit_scripts> to .profile?'
-echo '<y/enter/n>'
-read yesno
+read -r -p '<y/enter/n>' yesno
 if [ -z "${yesno}" ] || [ "${yesno}" = "y" ]; then
     echo 'Adding convenience functions to .profile'
     reset_shell='yes'
 
-    cp ${HOME}/.profile ${HOME}/.profile.copy
-    echo >> ${HOME}/.profile
-    echo '# Astrohack convenience functions:' >> ${HOME}/.profile
-    echo >> ${HOME}/.profile
-    echo 'activate_astrohack () {' >> ${HOME}/.profile
-    echo '    source '${env_address}'/bin/activate' >> ${HOME}/.profile
-    echo '}' >> ${HOME}/.profile
-    echo  >> ${HOME}/.profile
-    echo 'get_locit_scripts () {' >> ${HOME}/.profile
-    echo '    echo Downloading CASA pre locit script...' >> ${HOME}/.profile
-    echo '    wget https://github.com/casangi/astrohack/raw/main/etc/casa/pre-locit-script.py' >> ${HOME}/.profile
-    echo '    echo' >> ${HOME}/.profile
-    echo '    echo Downloading astrohack locit script...' >> ${HOME}/.profile
-    echo '    echo wget https://github.com/casangi/astrohack/raw/main/etc/casa/pre-locit-script.py' >> ${HOME}/.profile
-    echo '}' >> ${HOME}/.profile
+    cp "${HOME}"/.profile "${HOME}"/.profile.copy
+    {
+      echo
+      echo '# Astrohack convenience functions:'
+      echo
+      echo 'activate_astrohack () {'
+      echo '    source '"${env_address}"'/bin/activate'
+      echo '}'
+      echo
+      echo 'get_locit_scripts () {'
+      echo '    echo Downloading CASA pre locit script...'
+      echo '    wget https://github.com/casangi/astrohack/raw/main/etc/casa/pre-locit-script.py'
+      echo '    echo'
+      echo '    echo Downloading astrohack locit script...'
+      echo '    echo wget https://github.com/casangi/astrohack/raw/main/etc/casa/pre-locit-script.py'
+      echo '}'
+    } >> "${HOME}"/.profile
     
 else
     echo 'NOPE!'
