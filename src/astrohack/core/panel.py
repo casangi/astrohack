7fa1b955268036db1c76dd284e82c5697067a8fe
Victor de Souza magalhaes <vdesouza@nrao.edu>
2025-02-13 15:50:11 -0700

    Removed old code that dealt with data from AIPS from core/panel.py

--- src/astrohack/core/panel.py
+++ src/astrohack/core/panel.py
@@ -1,5 +1,4 @@
 import toolviper.utils.logger as logger
-import xarray as xr
 
 from astrohack.antenna.antenna_surface import AntennaSurface
 from astrohack.antenna.telescope import Telescope
@@ -14,27 +13,20 @@ def process_panel_chunk(panel_chunk_params):
     """
 
     clip_level = panel_chunk_params['clip_level']
-    if panel_chunk_params['origin'] == 'AIPS':
-        inputxds = xr.open_zarr(panel_chunk_params['image_name'])
-        telescope = Telescope(inputxds.attrs['telescope_name'])
-        antenna = inputxds.attrs['ant_name']
-        ddi = 0
-    else:
-        ddi = panel_chunk_params['this_ddi']
-        antenna = panel_chunk_params['this_ant']
-        inputxds = panel_chunk_params['xds_data']
-        logger.info(f'processing {create_dataset_label(antenna, ddi)}')
-        inputxds.attrs['AIPS'] = False
-        telescope = Telescope.from_xds(inputxds)
-        if isinstance(clip_level, dict):
-            ant_name = antenna.split('_')[1]
-            ddi_name = int(ddi.split('_')[1])
-            try:
-                clip_level = clip_level[ant_name][ddi_name]
-            except KeyError:
-                msg = f'Antenna {ant_name} and DDI {ddi_name} combination not found in clip_level dictionary'
-                logger.error(msg)
-                raise Exception(msg)
+    ddi = panel_chunk_params['this_ddi']
+    antenna = panel_chunk_params['this_ant']
+    inputxds = panel_chunk_params['xds_data']
+    logger.info(f'processing {create_dataset_label(antenna, ddi)}')
+    telescope = Telescope.from_xds(inputxds)
+    if isinstance(clip_level, dict):
+        ant_name = antenna.split('_')[1]
+        ddi_name = int(ddi.split('_')[1])
+        try:
+            clip_level = clip_level[ant_name][ddi_name]
+        except KeyError:
+            msg = f'Antenna {ant_name} and DDI {ddi_name} combination not found in clip_level dictionary'
+            logger.error(msg)
+            raise Exception(msg)
 
     needs_phase_wrapping_patch = data_from_version_needs_patch(panel_chunk_params['version'], '0.5.10')
 
