8c0c8ead074cab55c9403139eade2deb8107cf89
Victor de Souza magalhaes <vdesouza@nrao.edu>
2025-02-20 16:06:16 -0700

    Implemented comparison methods and plotting

--- src/astrohack/core/image_compare_tool.py
+++ src/astrohack/core/image_compare_tool.py
@@ -1,153 +1,127 @@
-from astropy.io import fits
 import numpy as np
 from scipy.interpolate import griddata
 from matplotlib import pyplot as plt
+
+from astrohack import compute_extent, data_statistics, statistics_to_text
+from astrohack.utils import are_axes_equal
+from astrohack.utils.algorithms import create_aperture_mask
 from astrohack.visualization.plot_tools import well_positioned_colorbar
 from astrohack.visualization.plot_tools import close_figure, get_proper_color_map
-from astrohack.utils.fits import read_fits, put_axis_in_fits_header, write_fits
+from astrohack.utils.fits import read_fits, put_axis_in_fits_header, write_fits, get_axis_from_fits_header
 import datetime
 
-# parser = argparse.ArgumentParser(description="Compare aperture FTIS maps produced by AIPS and AstroHACK\n"
-#                                  "Beam maps are not supported")
-# parser.add_argument('first', type=str, help='first aperture FITS (usually AIPS)')
-# parser.add_argument('second', type=str,
-#                     help='Second aperture FITS (usually AstroHACK)')
-# parser.add_argument('-n', '--noise-map', action='store_true',
-#                     default=False, help='Save noise images')
-# parser.add_argument('-c', '--noise-clip', type=float, default=1,
-#                     help='Noise clipping level (in sigmas)')
-# parser.add_argument('-q', '--quiet', action='store_true', default=False,
-#                     help='Do not print value to screen')
-# parser.add_argument('-d', '--diameter', type=float, default=25.0,
-#                     help='Dish diameter')
-# parser.add_argument('-b', '--blocage', type=float, default=2.0,
-#                     help='Dish inner blocage radius')
-# parser.add_argument('-m', '--colormap', type=str, default='viridis',
-#                     help='Colormap for non residual maps')
-# parser.add_argument('-p', '--no-division', action='store_true', default=False,
-#                     help='Do not perform division, i.e. factor assumed to be 1')
-# parser.add_argument('-s', '--shadow-width', type=float, default=1.5,
-#                     help='Arm shadow width in meters')
-# parser.add_argument('-r', '--shadow-rotation', type=float, default=0,
-#                      help='Arm shadow rotation in degrees,(e.g. 45 degress for some ALMA antennas)')
-# parser.add_argument('-z', '--first-zscale', type=float, nargs=2, default=[None, None],
-#                     help='Z scale for first image (min max)')
-# parser.add_argument('-y', '--second-zscale', type=float, nargs=2, default=[None, None],
-#                     help='Z scale for second image (min max)')
-# parser.add_argument('-f', '--fits', action='store_true',
-#                     default=False, help='Save products as FITS images')
-#
-# args = parser.parse_args()
+
+def test_image(fits_image):
+    if isinstance(fits_image, FITSImage):
+        pass
+    else:
+        raise TypeError('Reference image is not a FITSImage object')
 
 
-class image:
+class FITSImage:
 
-    def __init__(self, filename, noise_level, inlim, oulim, no_division, arm_width, arm_angle):
-        self.no_division = no_division
-        self.inlim = inlim
-        self.oulim = oulim
-        self.noise_level = noise_level
-        self.arm_width = arm_width
-        self.arm_angle = arm_angle*np.pi/180
+    def __init__(self, filename, telescope_obj):
+        self.telescope = telescope_obj
+        self.header, data = read_fits(filename)
+        self.data = data[0, 0, :, :]
 
-        opn_fits =  fits.open(filename)
-        self.data = opn_fits[0].data[0,0,:,:]
-        self._get_info_from_header(opn_fits[0].header)
-        opn_fits.close()
+        self.x_axis, _, self.x_unit = get_axis_from_fits_header(self.header, 1)
+        self.y_axis, _, self.y_unit = get_axis_from_fits_header(self.header, 2)
 
-        self._mask_image()
+        self.unit = self.header['BUNIT']
 
-        self.division = None
-        self.residuals = None
-        self.resampled = False
-        self.res_mean = None
-        self.res_rms = None
-        self.rootname = '.'.join(filename.split('.')[:-1])
-
-
-    def _astrohack_specific_init(self, header):
-        self.x_unit = header['CUNIT1']
-        self.y_unit = header['CUNIT2']
-        version = header['ORIGIN'].split()[1][:-1]
-        if test_version('v0.4.1', version) <= 0:
-            # This treatment is only necessary before v0.4.2
-            wavelength = header['WAVELENG']
-            self.x_axis *= wavelength
-            self.y_axis *= wavelength
-            self.data = np.fliplr(np.flipud(self.data))
-        if test_version('v0.5.0', version) >= 0:
-            print('estoy aqui?')
-            wavelength = header['WAVELENG']
-            self.x_axis /= wavelength
-            self.y_axis /= wavelength
-            self.data = np.fliplr(np.flipud(self.data))
-
-
-    def _get_info_from_header(self, header):
-        self.header = header
-        self.x_axis = get_axis_from_header(header, 1)
-        self.y_axis = get_axis_from_header(header, 2)
-        self.unit = header['BUNIT']
-
-        if 'AIPS' in header['ORIGIN']:
+        if 'AIPS' in self.header['ORIGIN']:
             self.x_unit = 'm'
             self.y_unit = 'm'
-        elif 'Astrohack' in header['ORIGIN']:
-            self._astrohack_specific_init(header)
+        elif 'Astrohack' in self.header['ORIGIN']:
+            self.data = self.data.T
         else:
-            raise Exception(f'Unrecognized origin:\n{header["origin"]}')
+            raise Exception(f'Unrecognized origin:\n{self.header["origin"]}')
 
+        self.base_mask = create_aperture_mask(self.x_axis, self.y_axis, self.telescope.inlim, self.telescope.oulim,
+                                              arm_width=self.telescope.arm_shadow_width,
+                                              arm_angle=self.telescope.arm_shadow_rotation)
+        self.rootname = '.'.join(filename.split('.')[:-1])
+        self.factor = 1.0
+        self.residuals = None
+        self.residuals_percent = None
+        self.divided_image = None
+        self.reference_name = None
 
     def resample(self, ref_image):
-        x_mesh_orig, y_mesh_orig = np.meshgrid(self.x_axis, self.y_axis)
-        x_mesh_dest, y_mesh_dest = np.meshgrid(ref_image.x_axis, ref_image.y_axis)
-        resamp = griddata((x_mesh_orig.ravel(), y_mesh_orig.ravel()),
-                          self.data.ravel(),
+        test_image(ref_image)
+        x_mesh_orig, y_mesh_orig = np.meshgrid(self.x_axis, self.y_axis, indexing='ij')
+        x_mesh_dest, y_mesh_dest = np.meshgrid(ref_image.x_axis, ref_image.y_axis, indexing='ij')
+        resamp = griddata((x_mesh_orig.ravel(), y_mesh_orig.ravel()), self.data.ravel(),
                           (x_mesh_dest.ravel(), y_mesh_dest.ravel()),
                           method='linear')
         size = ref_image.x_axis.shape[0], ref_image.y_axis.shape[0]
         self.x_axis = ref_image.x_axis
         self.y_axis = ref_image.y_axis
         self.data = resamp.reshape(size)
-        self._mask_image()
-        self.resampled = True
+        self.base_mask = create_aperture_mask(self.x_axis, self.y_axis, self.telescope.inlim, self.telescope.oulim,
+                                              arm_width=self.telescope.arm_shadow_width,
+                                              arm_angle=self.telescope.arm_shadow_rotation)
 
+    def compare_difference(self, ref_image):
+        test_image(ref_image)
+        if not self.image_has_same_sampling(ref_image):
+            self.resample(ref_image)
 
-    def _noise_filter(self):
-        noise = np.where(self.radius < self.oulim, np.nan, self.data)
-        noise = np.where(self.radius < self.inlim, self.data, noise)
-        noiserms = np.sqrt(np.nanmean(noise**2))
-        return noise, noiserms
+        self.residuals = ref_image.data - (self.data * self.factor)
+        self.residuals_percent = 100 * self.residuals/ref_image.data
+        self.reference_name = ref_image.rootname
 
+    def compare_scaled_difference(self, ref_image, rejection=10):
+        test_image(ref_image)
+        if not self.image_has_same_sampling(ref_image):
+            self.resample(ref_image)
+        simple_division = ref_image.data / self.data
+        rough_factor = np.nanmean(simple_division[self.base_mask])
+        self.divided_image = np.where(np.abs(simple_division) > rejection*rough_factor, np.nan, simple_division)
+        self.factor = np.nanmedian(self.divided_image)
+        self.compare_difference(ref_image)
 
-    def make_comparison(self, image2):
-        if self.no_division:
-            self.factor = 1.0
-            self.division = None
-        else:
-            self._compute_divide(image2)
+    def image_has_same_sampling(self, ref_image):
+        test_image(ref_image)
+        return are_axes_equal(self.x_axis, ref_image.x_axis) and are_axes_equal(self.y_axis, ref_image.y_axis)
 
-        self._compute_residuals(image2)
+    def _mask_array(self, image_array):
+        return np.where(self.base_mask, image_array, np.nan)
 
+    def plot_results(self, destination, plot_data=False, plot_percentuals=False, plot_divided_image=False,
+                     colormap='viridis', dpi=300, display=False):
 
-    def _compute_divide(self, image2):
-        division = self.masked/image2.masked
-        rude_factor = np.abs(np.nanmean(division))
-        self.division = np.where(np.abs(division)>10*rude_factor, np.nan, division)
-        self.factor = np.nanmedian(self.division)
+        extent = compute_extent(self.x_axis, self.y_axis, 0.0)
+        cmap = get_proper_color_map(colormap)
+        base_name = f'{destination}/{self.rootname}'
 
+        if self.residuals is None:
+            raise Exception("Cannot plot results as they don't exist yet.")
 
-    def _compute_residuals(self, image2, blanking=100):
-         percent = 100*(self.masked-self.factor*image2.masked)/self.masked
-         percent = np.where(np.abs(percent)>blanking, np.nan, percent)
-         self.residuals = percent
-         self.res_mean = np.nanmean(percent)
-         self.res_rms = np.sqrt(np.nanmean(percent**2))
+        self._plot_map(self._mask_array(self.residuals), 'Residuals', f'Residuals [{self.unit}]',
+                       f'{base_name}residuals.png', cmap, extent, 'Symmetrical', dpi, display,
+                       add_statistics=True)
 
+        if plot_data:
+            self._plot_map(self._mask_array(self.residuals), 'Original Data', f'Data [{self.unit}]',
+                           f'{base_name}data.png', cmap, extent, [None, None], dpi, display,
+                           add_statistics=False)
 
-    def _plot_map(self, data, title, label, filename, cmap, extent, zscale):
-        fig, ax = plt.subplots(1, 1, figsize=[10,8])
-        cmap = get_proper_color_map(cmap)
+        if plot_percentuals:
+            self._plot_map(self._mask_array(self.residuals_percent), 'Residuals in %', f'Residuals [%]',
+                           f'{base_name}residuals_percent.png', cmap, extent, 'Symmetrical', dpi, display,
+                           add_statistics=True)
+
+        if plot_divided_image:
+            if self.divided_image is None:
+                raise Exception("Cannot plot a divided image that does not exist.")
+            self._plot_map(self._mask_array(self.divided_image), 'Divided image', f'Division [ ]',
+                           f'{base_name}divided.png', cmap, extent, [None, None], dpi, display,
+                           add_statistics=True)
+
+    def _plot_map(self, data, title, zlabel, filename, cmap, extent, zscale, dpi, display, add_statistics=False):
+        fig, ax = plt.subplots(1, 1, figsize=[10, 8])
         if zscale == 'symmetrical':
             scale = max(np.abs(np.nanmin(data)), np.abs(np.nanmax(data)))
             vmin, vmax = -scale, scale
@@ -160,73 +134,37 @@ class image:
 
         im = ax.imshow(data, cmap=cmap, interpolation="nearest", extent=extent,
                        vmin=vmin, vmax=vmax,)
-        well_positioned_colorbar(ax, fig, im, label, location='right', size='5%', pad=0.05)
+        well_positioned_colorbar(ax, fig, im, zlabel, location='right', size='5%', pad=0.05)
         ax.set_xlabel(f"X axis [{self.x_unit}]")
         ax.set_ylabel(f"Y axis [{self.y_unit}]")
-        close_figure(fig, title, filename, 300, False)
+        if add_statistics:
+            data_stats = data_statistics(data)
+            ax.set_title(statistics_to_text(data_stats))
+        close_figure(fig, title, filename, dpi, display)
 
-
-    def plot(self, plot_noise, cmap, z_scale):
-        extent = [np.min(self.x_axis), np.max(self.x_axis), np.min(self.y_axis), np.max(self.y_axis)]
-        png = '.png'
-        if self.resampled:
-            title = f'Resampled {self.rootname}'
-            filename = f'{self.rootname}.resampled'
-        else:
-            title = f'{self.rootname}'
-            filename = f'{self.rootname}'
-
-        if self.no_division:
-            zlabel = f'?What is type? [{self.unit}]'
-        else:
-            zlabel = f'Amplitude [{self.unit}]'
-
-        self._plot_map(self.masked, title, zlabel, filename+png, cmap, extent, z_scale)
-        self._plot_map(self.mask, f'Mask used for {self.rootname}', 'Mask value',
-                       f'{self.rootname}.mask{png}', cmap, extent, [0, 1])
-
-        if self.division is not None:
-            self._plot_map(self.division, f'Division map, mean factor:{self.factor:.3}',
-                           'Divided value [ ]',
-                           f'{filename}.division{png}', cmap, extent, [None, None])
-
-        if self.residuals is not None:
-            self._plot_map(self.residuals,
-                           f'Residual map, mean residual: {self.res_mean:.3}%, residual RMS: '
-                           f'{self.res_rms:.3}%',
-                           'Residuals [%]',
-                           f'{filename}.residual{png}', 'RdBu_r', extent, 'symmetrical')
-
-        if plot_noise:
-            if self.noise is not None:
-                self._plot_map(self.noise,
-                               f'Noise like component for {self.rootname}', zlabel,
-                               f'{filename}.noise{png}', cmap, extent, [None, None])
-
-
-    def to_fits(self):
-        fits = '.fits'
-        header = self.header.copy()
-        put_axis_in_header(self.x_axis, self.x_unit, 1, header)
-        put_axis_in_header(self.y_axis, self.y_unit, 2, header)
-
-        if self.resampled:
-            filename = f'comp_{self.rootname}.resampled'
-        else:
-            filename = f'comp_{self.rootname}'
-
-        create_fits(header, self.masked, f'{filename}.masked{fits}')
-        create_fits(header, self.mask, f'{filename}.mask{fits}')
-
-        if self.division is not None:
-            create_fits(header, self.division, f'{filename}.division{fits}')
-
-        if self.residuals is not None:
-            create_fits(header, self.residuals, f'{filename}.residual{fits}')
-
-
-        if self.noise is not None:
-            create_fits(header, self.noise, f'{filename}.noise{fits}')
+    # def to_fits(self):
+    #     fits = '.fits'
+    #     header = self.header.copy()
+    #     put_axis_in_header(self.x_axis, self.x_unit, 1, header)
+    #     put_axis_in_header(self.y_axis, self.y_unit, 2, header)
+    #
+    #     if self.resampled:
+    #         filename = f'comp_{self.rootname}.resampled'
+    #     else:
+    #         filename = f'comp_{self.rootname}'
+    #
+    #     create_fits(header, self.masked, f'{filename}.masked{fits}')
+    #     create_fits(header, self.mask, f'{filename}.mask{fits}')
+    #
+    #     if self.division is not None:
+    #         create_fits(header, self.division, f'{filename}.division{fits}')
+    #
+    #     if self.residuals is not None:
+    #         create_fits(header, self.residuals, f'{filename}.residual{fits}')
+    #
+    #
+    #     if self.noise is not None:
+    #         create_fits(header, self.noise, f'{filename}.noise{fits}')
 
 
     def print_stats(self):
