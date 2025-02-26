3c484f1eed091e498d77a1e2d68eea28b24d66f2
Victor de Souza magalhaes <vdesouza@nrao.edu>
2025-02-26 16:06:12 -0700

    Some progress towards controlling the image comparison tool

--- src/astrohack/image_comparison_tool.py
+++ src/astrohack/image_comparison_tool.py
@@ -1,9 +1,15 @@
+from typing import Union, List
+import xarray as xr
+import pathlib
+
+import toolviper.utils.logger as logger
+
 from astrohack.core.image_compare_tool import image_comparison_chunk
 
 
 def compare_fits_image(
-        image: str,
-        reference_image: str,
+        image: Union[str, List[str]],
+        reference_image: Union[str, List[str]],
         telescope_name: str,
         destination: str,
         comparison: str = 'direct',
@@ -16,9 +22,19 @@ def compare_fits_image(
         dpi: int = 300,
         display: bool = False
 ):
+
+    if isinstance(image, str):
+        image = [image]
+    if isinstance(reference_image, str):
+        reference_image = [reference_image]
+    if len(image) != len(reference_image):
+        msg = 'List of reference images has a different size from the list of images'
+        logger.error(msg)
+        return
+
     param_dict = locals()
-
-    param_dict['this_image'] = image
-    param_dict['this_reference_image'] = reference_image
-
-    image_comparison_chunk(param_dict)
+    pathlib.Path(param_dict['destination']).mkdir(exist_ok=True)
+    for i_img in range(len(image)):
+        param_dict['this_image'] = image[i_img]
+        param_dict['this_reference_image'] = reference_image[i_img]
+        image_comparison_chunk(param_dict)
