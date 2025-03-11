2eeb9218ae3cbf61d68fd4d89cd27ede5e696cb4
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2025-03-11 14:03:21 -0600

    Made qps fitting jit for significant speedup

--- src/astrohack/utils/ray_tracing_general.py
+++ src/astrohack/utils/ray_tracing_general.py
@@ -1,5 +1,6 @@
 import numpy as np
-from astrohack.utils.algorithms import least_squares
+from astrohack.utils.algorithms import least_squares_jit
+from numba import njit
 
 
 def generalized_dot(vec_map_a, vec_map_b):
@@ -18,6 +19,7 @@ def normalize_vector_map(vector_map):
 def reflect_light(light, normals):
     return light - 2 * generalized_dot(light, normals)[..., np.newaxis] * normals
 
+@njit(cache=True, nogil=True)
 def compute_quintic_pseudo_spline_coefficients(point_cloud):
     # QPS definition from Bergman et al. 1994, IEEE Transactions on Antennas and propagation
     def dist_2d(pnt_a, pnt_b):
@@ -29,7 +31,7 @@ def compute_quintic_pseudo_spline_coefficients(point_cloud):
 
     npnt = pcd_xy.shape[0]
     n_var = npnt + n_extra_coeffs
-    matrix_shape = [n_var, n_var]
+    matrix_shape = (n_var, n_var)
 
     matrix = np.zeros(matrix_shape)
     vector = np.zeros(n_var)
@@ -37,6 +39,7 @@ def compute_quintic_pseudo_spline_coefficients(point_cloud):
     # Building system's right-hand side vector
     vector[0:npnt] = pcd_z
 
+    print()
     # Building system matrix
     for irow in range(npnt):
         for icol in range(npnt):
@@ -49,10 +52,12 @@ def compute_quintic_pseudo_spline_coefficients(point_cloud):
         matrix[irow, npnt+4] = pcd_xy[irow, 1]
         matrix[irow, npnt+5] = 1
 
+        print('\033[F', (irow+1)/npnt*100, '%              ')
+
     for irow in range(npnt, n_var):
         matrix[irow, 0:npnt] = 1
 
-    qps_coeffs, _, _ = least_squares(matrix, vector)
+    qps_coeffs, _, _, _ = least_squares_jit(matrix, vector)
     return qps_coeffs
 
 def compute_qps_value(pnt, qps_coeffs, point_cloud):
@@ -61,11 +66,22 @@ def compute_qps_value(pnt, qps_coeffs, point_cloud):
     pcd_xy = point_cloud[:, 0:2]
     a_coeffs = qps_coeffs[0:npnt]
     b_coeffs = qps_coeffs[npnt:]
-    r_term = np.sum(a_coeffs*np.sqrt(pnt[np.newaxis, 0:2]-pcd_xy)**5)
+    r_term = np.sum(a_coeffs*np.sqrt(np.sum((pnt[np.newaxis, 0:2]-pcd_xy)**2, axis=1))**5)
 
     z_val  = r_term + b_coeffs[0]*pnt[0]**2 + b_coeffs[1]*pnt[0]*pnt[1] + b_coeffs[2]*pnt[1]**2
     z_val += b_coeffs[3]*pnt[0] + b_coeffs[4]*pnt[1] + b_coeffs[5]
 
     return z_val
+
+def qps_pcd_fitting(point_cloud_filename, output_coeff_filename, max_rows=None):
+
+    pcd_data = np.loadtxt(point_cloud_filename, max_rows=max_rows)
+    qps_coeffs = compute_quintic_pseudo_spline_coefficients(pcd_data)
+
+    np.save(output_coeff_filename, qps_coeffs)
+
+    return qps_coeffs
+
+
 
 
