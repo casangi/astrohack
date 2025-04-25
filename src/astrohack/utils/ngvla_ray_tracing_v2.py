f8dcc69fb43f7e0f7d7ec975b4ed20d9b27b3ead
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2025-04-25 11:08:05 -0600

    Implemented map plotting

--- src/astrohack/utils/ngvla_ray_tracing_v2.py
+++ src/astrohack/utils/ngvla_ray_tracing_v2.py
@@ -1,7 +1,5 @@
-import numpy as np
-
 from astrohack.utils.ray_tracing_general import *
-
+from astrohack.visualization.plot_tools import *
 
 class NgvlaRayTracer:
 
@@ -60,7 +58,32 @@ class NgvlaRayTracer:
 
         self.grd_sc_reflections = reflect_light(self.grd_pm_reflections, self.grd_sc_normals)
 
+    def plot_key(self, key, colormap='viridis', zlim=None, dpi=300):
+        axis_labels = ['X', 'Y', 'Z']
+        if 'grd_' not in key:
+            raise Exception(f'{key} is not a plottable map.')
 
+        degridded = self.__dict__[key]
+
+        regridded = regrid_array(self.x_axis, self.y_axis, degridded, self.grd_idx)
+        cmap = get_proper_color_map(colormap)
+
+        if regridded.ndim == 3: # [nx,ny, 2|3]
+            third_dim = regridded.shape[2]
+            if third_dim == 3:
+                fig_size = [18, 10]
+            else: # third_dim == 2
+                fig_size = [14, 10]
+            fig, ax = create_figure_and_axes(fig_size, [1, third_dim])
+
+            for iax in range(third_dim):
+                simple_imshow_map_plot(ax[iax], fig, self.x_axis, self.y_axis, regridded[:, :, iax],
+                                       f'{axis_labels[iax]} axis', cmap, zlim)
+        else: # [nx, ny]
+            fig, ax = create_figure_and_axes(None, [1, 1])
+            simple_imshow_map_plot(ax, fig, self.x_axis, self.y_axis, regridded, '', cmap, zlim)
+
+        close_figure(fig, f'{key} map', f'{key}_map.png', dpi, False)
 
 
 
