68a8ce07f126e39e3d09b49e8a054682151a22e8
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2025-06-20 11:19:04 -0600

    implemented making a gridded image of the primary and the point inclination with respect to light direction.

--- src/astrohack/utils/ray_tracing_general.py
+++ src/astrohack/utils/ray_tracing_general.py
@@ -1,4 +1,7 @@
 import numpy as np
+
+from astrohack import create_coordinate_images, regrid_data_onto_2d_grid
+from astrohack.utils import create_2d_array_reconstruction_array
 from astrohack.utils.algorithms import least_squares
 
 nanvec3d = np.array([np.nan, np.nan, np.nan])
@@ -115,14 +118,25 @@ class LocalQPS:
         self.local_qps_coeffs = None
         self.local_pcds = None
 
+        self.current_z_val = None
+        self.current_z_cos = None
+        self.current_u_axis = None
+        self.current_v_axis = None
+
+        self.high_res_z_val = None
+        self.high_res_z_cos = None
+        self.high_res_u_axis = None
+        self.high_res_v_axis = None
+
     @classmethod
-    def from_pcd(cls, pcd_data, local_qps_n_pnt=20):
+    def from_pcd(cls, pcd_data, local_qps_n_pnt=20, displacement=(0,0,0)):
         new_obj = cls()
-        new_obj._init_from_pcd(pcd_data, local_qps_n_pnt)
+        new_obj._init_from_pcd(pcd_data, local_qps_n_pnt, displacement)
         return new_obj
 
-    def _init_from_pcd(self, pcd_data, local_qps_n_pnt):
+    def _init_from_pcd(self, pcd_data, local_qps_n_pnt, displacement):
         self.global_pcd = pcd_data
+        self.global_pcd -= np.array(displacement)
         self.npnt = self.global_pcd.shape[0]
         self.local_qps_n_pnt = local_qps_n_pnt
         self.local_pcds = np.empty([self.npnt, self.local_qps_n_pnt, 3])
@@ -152,7 +166,7 @@ class LocalQPS:
         """
         return
 
-    def compute_zval_and_z_angle(self, point):
+    def compute_z_val_and_z_cos(self, point):
         """
         Idea is to compute value of QPS and angle with boresight
         Args:
@@ -161,3 +175,48 @@ class LocalQPS:
         Returns:
 
         """
+        dist = generalized_dist(self.global_pcd[:, 0:2], point)
+        i_closest = np.argsort(dist)[0]
+        z_val, normal = qps_compute_point_and_normal(point, self.local_qps_coeffs[i_closest], self.local_pcds[i_closest])
+
+        return z_val, normal
+
+    def plot_z_val_and_z_cos(self, colormap, zlim, dpi, display):
+        return
+
+    def compute_gridded_z_val_and_z_cos(self, u_axis, v_axis, mask, gridding_engine='2D regrid', light=(0,0,-1)):
+        light = np.array(light)
+
+        u_mesh, v_mesh = create_coordinate_images(u_axis, v_axis)
+        uv_idx_grid = create_2d_array_reconstruction_array(u_axis, v_axis, mask)
+
+        uv_points = np.empty_like(uv_idx_grid)
+        uv_points[:, 0] = u_mesh[mask]
+        uv_points[:, 1] = v_mesh[mask]
+
+        z_val = np.empty([uv_points.shape[0]])
+        z_norm = np.empty([uv_points.shape[0], 3])
+
+        for ip, point in enumerate(uv_points):
+            z_val[ip], z_norm[ip] = self.compute_z_val_and_z_cos(point)
+
+        z_angle = (generalized_dot(z_norm, light))/(generalized_norm(z_norm)*generalized_norm(light))
+
+        if gridding_engine == '2D regrid':
+            z_val_grid = regrid_data_onto_2d_grid(u_axis, v_axis, z_val, uv_idx_grid)
+            z_cos_grid = regrid_data_onto_2d_grid(u_axis, v_axis, z_angle, uv_idx_grid)
+        else:
+            raise Exception('only 2D regrid available now')
+
+        self.current_z_val=z_val_grid
+        self.current_z_cos=z_cos_grid
+        self.current_u_axis = u_axis
+        self.current_v_axis = v_axis
+
+        return z_val_grid, z_cos_grid
+
+    def downgrid_high_resolution_z_val_and_z_cos(self, u_axis, v_axis, gridding_engine):
+        return
+
+    def compute_high_resolution_z_val_and_z_cos(self, x_resolution, y_resolution):
+        return
