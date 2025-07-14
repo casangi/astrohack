ed27e0be716ff3e32f93457493e8af4ce003d728
Victor de Souza magalhaes <vdesouza@nrao.edu>
2025-07-14 15:02:40 -0600

    Made the panel center and screw plotting positions correct.

--- src/astrohack/antenna/ring_panel.py
+++ src/astrohack/antenna/ring_panel.py
@@ -58,7 +58,7 @@ class RingPanel(BasePanel):
         self.first = ipanel == 0
         zeta = (ipanel + 0.5) * angle
         rt = (self.inrad + self.ourad) / 2
-        self.center = PanelPoint(rt * np.cos(zeta), rt * np.sin(zeta))
+        self.center = PanelPoint(-rt * np.sin(zeta), -rt * np.cos(zeta))
         screws = self._init_screws(screw_scheme, screw_offset)
         plot_screw_pos = self._init_screws(screw_scheme, 2 * plot_screw_size)
         fi = self.theta2 - self.theta1
@@ -108,7 +108,7 @@ class RingPanel(BasePanel):
                 else:
                     theta = self.theta2 - deltatheta
                 screws[iscrew] = PanelPoint(
-                    radius * np.cos(theta), radius * np.sin(theta)
+                    -radius * np.sin(theta), -radius * np.cos(theta)
                 )
         return screws
 
