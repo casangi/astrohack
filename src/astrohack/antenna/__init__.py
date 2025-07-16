43de2b55c6f2f090db8d80881870cdb77b904424
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2025-07-16 11:36:04 -0600

    Changed import scopes so that only user facing routines are available at the package level.

--- src/astrohack/antenna/__init__.py
+++ src/astrohack/antenna/__init__.py
@@ -1,2 +1,5 @@
-from .antenna_surface import *
-from .telescope import *
+from .antenna_surface import AntennaSurface
+from .base_panel import BasePanel
+from .polygon_panel import PolygonPanel
+from .ring_panel import RingPanel
+from .telescope import get_proper_telescope
