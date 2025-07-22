c22836db02f8fa9f450e7bed85e6e130e6f6998d
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2025-07-22 15:58:23 -0600

    Added test_grid_linear_beam_chan_average, test_grid_gaussian_beam_no_chan_average and test_grid_gaussian_beam_chan_average.

--- tests/unit/test_gridding.py
+++ tests/unit/test_gridding.py
@@ -83,7 +83,124 @@ class TestGridAlgorithms:
         assert not grid_corr, "Linear beam griddind does not warrant a gridding correction in aperture plane"
         assert obs_sum == self.obs_sum, "Observation summary differs from the expected"
 
+        return
 
+    def test_grid_linear_beam_chan_average(self):
+        beam_grid, time_centroid, output_freq_axis, pol_axis, l_axis, m_axis, grid_corr, obs_sum = grid_beam(
+            ant_ddi_dict=self.ant_data_dict['ddi_0'],
+            grid_size=self.grid_size,
+            sky_cell_size=self.cell_size,
+            avg_chan=True,
+            telescope=self.vla,
+            chan_tol_fac=self.chan_tolerance_factor,
+            grid_interpolation_mode='linear',
+            observation_summary=self.obs_sum,
+            label='Test beam linear grid',
+        )
+
+        ref_freq_axis = np.average(self.ant_data_dict['ddi_0']['map_0'].chan.values)
+        expected_shape = [1, 1, 4, 28, 28]
+
+
+        ref_center_values = [0.2555271590565431+0.013577819885576325j,
+                             0.006690232199874844-0.0004646980470803373j,
+                             -0.008884407577565362+0.005418552363686883j,
+                             0.2589463980023335+0.013353365347169917j,
+                             ]
+
+        assert np.all(np.isclose(expected_shape, beam_grid.shape)), "Beam grid does not have the expected shape"
+
+        for i_pol, pol in enumerate(pol_axis):
+            assert np.isclose(beam_grid[0, 0, i_pol, 14, 14], ref_center_values[i_pol]), \
+                f'Center pixel for {pol} does not match reference'
+
+        assert np.isclose(time_centroid[0], self.expected_time_centroid), \
+            "Time centroid is different from the expected"
+        assert l_axis.shape[0] == self.grid_size[0], "Grid size and l axis size are not equal"
+        assert m_axis.shape[0] == self.grid_size[1], "Grid size and m axis size are not equal"
+        assert np.all(np.isclose(ref_freq_axis, output_freq_axis)), \
+            "Output frequency axis is not equal to the reference"
+        assert not grid_corr, "Linear beam griddind does not warrant a gridding correction in aperture plane"
+        assert obs_sum == self.obs_sum, "Observation summary differs from the expected"
 
         return
 
+    def test_grid_gaussian_beam_no_chan_average(self):
+        beam_grid, time_centroid, output_freq_axis, pol_axis, l_axis, m_axis, grid_corr, obs_sum = grid_beam(
+            ant_ddi_dict=self.ant_data_dict['ddi_0'],
+            grid_size=self.grid_size,
+            sky_cell_size=self.cell_size,
+            avg_chan=False,
+            telescope=self.vla,
+            chan_tol_fac=self.chan_tolerance_factor,
+            grid_interpolation_mode='gaussian',
+            observation_summary=self.obs_sum,
+            label='Test beam linear grid',
+        )
+
+        ref_freq_axis = self.ant_data_dict['ddi_0']['map_0'].chan.values
+        expected_shape = [1, 64, 4, 28, 28]
+
+
+        ref_center_values = [0.37580005477133005+0.019891983361750222j,
+                             0.013640750479123904+0.01227407036248688j,
+                             -0.011975895008132082+0.01375238260072988j,
+                             0.3630087962254655+0.015249794836502932j,
+                             ]
+
+        assert np.all(np.isclose(expected_shape, beam_grid.shape)), "Beam grid does not have the expected shape"
+
+        for i_pol, pol in enumerate(pol_axis):
+            assert np.isclose(beam_grid[0, 32, i_pol, 14, 14], ref_center_values[i_pol]), \
+                f'Center pixel for {pol} does not match reference'
+
+        assert np.isclose(time_centroid[0], self.expected_time_centroid), \
+            "Time centroid is different from the expected"
+        assert l_axis.shape[0] == self.grid_size[0], "Grid size and l axis size are not equal"
+        assert m_axis.shape[0] == self.grid_size[1], "Grid size and m axis size are not equal"
+        assert np.all(np.isclose(ref_freq_axis, output_freq_axis)), \
+            "Output frequency axis is not equal to the reference"
+        assert grid_corr, "Linear beam griddind does not warrant a gridding correction in aperture plane"
+        assert obs_sum == self.obs_sum, "Observation summary differs from the expected"
+
+        return
+
+    def test_grid_gaussian_beam_chan_average(self):
+        beam_grid, time_centroid, output_freq_axis, pol_axis, l_axis, m_axis, grid_corr, obs_sum = grid_beam(
+            ant_ddi_dict=self.ant_data_dict['ddi_0'],
+            grid_size=self.grid_size,
+            sky_cell_size=self.cell_size,
+            avg_chan=True,
+            telescope=self.vla,
+            chan_tol_fac=self.chan_tolerance_factor,
+            grid_interpolation_mode='gaussian',
+            observation_summary=self.obs_sum,
+            label='Test beam linear grid',
+        )
+
+        ref_freq_axis = np.average(self.ant_data_dict['ddi_0']['map_0'].chan.values)
+        expected_shape = [1, 1, 4, 28, 28]
+
+
+        ref_center_values = [0.33047897570338663+0.018859501335040586j,
+                             0.005984131375211977+0.0023096517403009068j,
+                             -0.005063436214969807+0.010002728960915877j,
+                             0.323584178032157+0.017291480615071117j,
+                             ]
+
+        assert np.all(np.isclose(expected_shape, beam_grid.shape)), "Beam grid does not have the expected shape"
+
+        for i_pol, pol in enumerate(pol_axis):
+            assert np.isclose(beam_grid[0, 0, i_pol, 14, 14], ref_center_values[i_pol]), \
+                f'Center pixel for {pol} does not match reference'
+
+        assert np.isclose(time_centroid[0], self.expected_time_centroid), \
+            "Time centroid is different from the expected"
+        assert l_axis.shape[0] == self.grid_size[0], "Grid size and l axis size are not equal"
+        assert m_axis.shape[0] == self.grid_size[1], "Grid size and m axis size are not equal"
+        assert np.all(np.isclose(ref_freq_axis, output_freq_axis)), \
+            "Output frequency axis is not equal to the reference"
+        assert grid_corr, "Linear beam griddind does not warrant a gridding correction in aperture plane"
+        assert obs_sum == self.obs_sum, "Observation summary differs from the expected"
+
+        return
