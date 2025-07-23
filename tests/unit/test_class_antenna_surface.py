afde38168dbd41df19d98519d314c94268937e82
Victor de Souza magalhaes <vdesouza@nrao.edu>
2025-07-23 09:48:33 -0600

    Fixed reference values in test_class_antenna_surface.py

--- tests/unit/test_class_antenna_surface.py
+++ tests/unit/test_class_antenna_surface.py
@@ -107,8 +107,9 @@ class TestClassAntennaSurface:
         """
         Tests that fitting results for two panels match the reference
         """
-        solveparsp0 = [-0.00037302, 0.00032415, -0.00092434]
-        solveparsp30 = [0.00039928, 0.00038105, -0.00067004]
+
+        solveparsp0 = [-0.00046085,  0.00024772, -0.00074025]
+        solveparsp30 = [0.00032915,  0.00024761, -0.00017642]
         self.tant.fit_surface()
 
         assert len(self.tant.panels[0].model.parameters) == len(solveparsp0), (
